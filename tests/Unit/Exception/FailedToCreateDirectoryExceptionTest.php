a39dc2ae8567a3cf6fa2acfd393a42b32d4c5084
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-19 19:25:20 -0600

    Create FailedToCreateDirectoryExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/FailedToCreateDirectoryExceptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\TestifyTests\Unit\Exception;
+
+use Ghostwriter\Testify\Exception\FailedToCreateDirectoryException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(FailedToCreateDirectoryException::class)]
+final class FailedToCreateDirectoryExceptionTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::markTestSkipped('TODO: Implement test.');
+    }
+}
