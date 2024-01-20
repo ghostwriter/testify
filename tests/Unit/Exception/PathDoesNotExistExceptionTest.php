bbacd3167230ac6e3eb93eceff33193e20cb413c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-19 22:47:45 -0600

    Create PathDoesNotExistExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/PathDoesNotExistExceptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\TestifyTests\Unit\Exception;
+
+use Ghostwriter\Testify\Exception\PathDoesNotExistException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(PathDoesNotExistException::class)]
+final class PathDoesNotExistExceptionTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
