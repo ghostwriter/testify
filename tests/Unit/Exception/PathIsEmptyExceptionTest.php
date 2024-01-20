997cf0a2d343301610778b2a11f836a6b6b7629f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-19 22:47:47 -0600

    Create PathIsEmptyExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/PathIsEmptyExceptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\TestifyTests\Unit\Exception;
+
+use Ghostwriter\Testify\Exception\PathIsEmptyException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(PathIsEmptyException::class)]
+final class PathIsEmptyExceptionTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
