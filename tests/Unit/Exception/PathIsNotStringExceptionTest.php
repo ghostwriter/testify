6b13c8d2a8a9c73b1b17ed76c2d40047566c02ff
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-19 22:48:01 -0600

    Create PathIsNotStringExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/PathIsNotStringExceptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\TestifyTests\Unit\Exception;
+
+use Ghostwriter\Testify\Exception\PathIsNotStringException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(PathIsNotStringException::class)]
+final class PathIsNotStringExceptionTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
