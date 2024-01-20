67a48c789c7c26326a1c4999fb6ba5f627466c64
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-19 22:47:39 -0600

    Create FailedToWriteFileExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/FailedToWriteFileExceptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\TestifyTests\Unit\Exception;
+
+use Ghostwriter\Testify\Exception\FailedToWriteFileException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(FailedToWriteFileException::class)]
+final class FailedToWriteFileExceptionTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
