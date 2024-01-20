468f54b511ef983b86f194834244511960f1ff0d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-19 22:48:07 -0600

    Create StubNotReadableExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/StubNotReadableExceptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\TestifyTests\Unit\Exception;
+
+use Ghostwriter\Testify\Exception\StubNotReadableException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(StubNotReadableException::class)]
+final class StubNotReadableExceptionTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
