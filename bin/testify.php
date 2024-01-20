8732feacd75baf205044164f2806c5edd7a0c0c0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-19 23:11:49 -0600

    Update testify.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- bin/testify.php
+++ bin/testify.php
@@ -4,21 +4,27 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Console;
 
-use Ghostwriter\Testify\TestifyCommand;
+use Ghostwriter\Testify\Command\TestifyCommand;
 
 use const STDERR;
 
 use function dirname;
+use function file_exists;
 use function fwrite;
 use function sprintf;
 
 /** @var ?string $_composer_autoload_path */
 (static function (string $composerAutoloadPath): void {
-    /** @psalm-suppress UnresolvableInclude */
-    require $composerAutoloadPath ?: fwrite(
-        STDERR,
-        sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath)
-    ) && exit(1);
+    if (! file_exists($composerAutoloadPath)) {
+        fwrite(
+            STDERR,
+            sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath)
+        );
+
+        exit(1);
+    }
+
+    require $composerAutoloadPath;
 
     /**
      * #BlackLivesMatter.
