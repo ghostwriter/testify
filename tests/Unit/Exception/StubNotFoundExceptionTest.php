1a9c6cb15d227d90f1a09a746d5c29717059d018
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-19 22:48:05 -0600

    Create StubNotFoundExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/StubNotFoundExceptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\TestifyTests\Unit\Exception;
+
+use Ghostwriter\Testify\Exception\StubNotFoundException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(StubNotFoundException::class)]
+final class StubNotFoundExceptionTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
