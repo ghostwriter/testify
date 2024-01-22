012b6619e2ea7fe704f6c4c0474cd9c21dbda4aa
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 17:46:15 -0600

    Create UseStatements.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/NodeVisitor/UseStatements.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\NodeVisitor;
+
+use function array_key_exists;
+
+final class UseStatements
+{
+    private array $uses = [];
+
+    public function get(string $name): UseStatement
+    {
+        return $this->uses[$name] ?? throw new UseStatementNotFoundException($name);
+    }
+
+    public function has(string $name): bool
+    {
+        return array_key_exists($name, $this->uses);
+    }
+
+    public function set(string $name): void
+    {
+        $this->uses[$name] = new UseStatement($name);
+    }
+
+    public function uses(): array
+    {
+        return $this->uses;
+    }
+}
