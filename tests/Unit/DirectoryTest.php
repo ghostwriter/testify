55ee159887331d83db2d499af46c35cdb2773026
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 18:13:24 -0600

    Update DirectoryTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/DirectoryTest.php
+++ tests/Unit/DirectoryTest.php
@@ -4,15 +4,49 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit;
 
+use Generator;
 use Ghostwriter\Testify\Directory;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\InputInterface;
 
 #[CoversClass(Directory::class)]
 final class DirectoryTest extends TestCase
 {
-    public function testExample(): void
+    protected function setUp(): void
+    {
+        self::markTestSkipped('Not implemented yet.');
+    }
+
+    #[DataProvider('dataProviderConstruct')]
+    public function testConstruct(string $path): void
     {
         self::assertTrue(true);
+    }
+
+    #[DataProvider('dataProviderNew')]
+    public function testNew(InputInterface $input): void
+    {
+        self::assertTrue(true);
+    }
+
+    public function testPath(): void
+    {
+        self::assertTrue(true);
+    }
+
+    public static function dataProviderConstruct(): Generator
+    {
+        yield from [
+            'example' => [true],
+        ];
+    }
+
+    public static function dataProviderNew(): Generator
+    {
+        yield from [
+            'example' => [true],
+        ];
     }
 }
