8bd71e5eb3977c33730c81b74068576e9b251180
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 18:14:46 -0600

    Create HashNodeVisitorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/NodeVisitor/HashNodeVisitorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\TestifyTests\Unit\NodeVisitor;
+
+use Generator;
+use Ghostwriter\Testify\NodeVisitor\HashNodeVisitor;
+use PhpParser\Node;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(HashNodeVisitor::class)]
+final class HashNodeVisitorTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        self::markTestSkipped('Not implemented yet.');
+    }
+
+    #[DataProvider('dataProviderEnterNode')]
+    public function testEnterNode(Node $node): void
+    {
+        self::assertTrue(true);
+    }
+
+    public static function dataProviderEnterNode(): Generator
+    {
+        yield from [
+            'example' => [true],
+        ];
+    }
+}
