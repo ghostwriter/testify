26ddc37b2189c3ae092bdd9d2675bfe92d476614
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 18:14:16 -0600

    Update StubNotFoundExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/StubNotFoundExceptionTest.php
+++ tests/Unit/Exception/StubNotFoundExceptionTest.php
@@ -11,6 +11,11 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(StubNotFoundException::class)]
 final class StubNotFoundExceptionTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        self::markTestSkipped('Not implemented yet.');
+    }
+
     public function testExample(): void
     {
         self::assertTrue(true);
