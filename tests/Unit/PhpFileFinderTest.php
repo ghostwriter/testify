62c92931c3e840986dd205a9aafe6ebbd4fd3a53
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 18:15:10 -0600

    Update PhpFileFinderTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/PhpFileFinderTest.php
+++ tests/Unit/PhpFileFinderTest.php
@@ -4,15 +4,30 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit;
 
+use Generator;
 use Ghostwriter\Testify\PhpFileFinder;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(PhpFileFinder::class)]
 final class PhpFileFinderTest extends TestCase
 {
-    public function testExample(): void
+    protected function setUp(): void
+    {
+        self::markTestSkipped('Not implemented yet.');
+    }
+
+    #[DataProvider('dataProviderFind')]
+    public function testFind(string $path, Closure $match, Closure $skip): void
     {
         self::assertTrue(true);
+    }
+
+    public static function dataProviderFind(): Generator
+    {
+        yield from [
+            'example' => [true],
+        ];
     }
 }
