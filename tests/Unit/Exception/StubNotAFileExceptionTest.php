8d1290ce6941c41d44807b9051d17068995f8574
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 18:14:14 -0600

    Update StubNotAFileExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/StubNotAFileExceptionTest.php
+++ tests/Unit/Exception/StubNotAFileExceptionTest.php
@@ -11,6 +11,11 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(StubNotAFileException::class)]
 final class StubNotAFileExceptionTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        self::markTestSkipped('Not implemented yet.');
+    }
+
     public function testExample(): void
     {
         self::assertTrue(true);
