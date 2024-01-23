55485b173a5860cf9d69f2c0b4402be5bff0d113
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 18:03:14 -0600

    Update UseStatements.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/NodeVisitor/UseStatements.php
+++ src/NodeVisitor/UseStatements.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\NodeVisitor;
 
+use Ghostwriter\Testify\Exception\UseStatementNotFoundException;
 use function array_key_exists;
 
 final class UseStatements
