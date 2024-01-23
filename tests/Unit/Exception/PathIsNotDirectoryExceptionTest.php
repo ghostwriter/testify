c01b592beac2378626354085eb771bc857852c48
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 18:14:07 -0600

    Update PathIsNotDirectoryExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/PathIsNotDirectoryExceptionTest.php
+++ tests/Unit/Exception/PathIsNotDirectoryExceptionTest.php
@@ -11,6 +11,11 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(PathIsNotDirectoryException::class)]
 final class PathIsNotDirectoryExceptionTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        self::markTestSkipped('Not implemented yet.');
+    }
+
     public function testExample(): void
     {
         self::assertTrue(true);
