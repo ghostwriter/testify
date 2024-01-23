fafbc8e72294ccde1ab77df66cfe834c53733508
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 18:14:10 -0600

    Update PathIsNotStringExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/PathIsNotStringExceptionTest.php
+++ tests/Unit/Exception/PathIsNotStringExceptionTest.php
@@ -11,6 +11,11 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(PathIsNotStringException::class)]
 final class PathIsNotStringExceptionTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        self::markTestSkipped('Not implemented yet.');
+    }
+
     public function testExample(): void
     {
         self::assertTrue(true);
