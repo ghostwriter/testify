a181389c9acdf64a531e3c9c588cea4b29a08310
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-22 18:13:43 -0600

    Update FailedToWriteFileExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToWriteFileExceptionTest.php
+++ tests/Unit/Exception/FailedToWriteFileExceptionTest.php
@@ -11,6 +11,11 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(FailedToWriteFileException::class)]
 final class FailedToWriteFileExceptionTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        self::markTestSkipped('Not implemented yet.');
+    }
+
     public function testExample(): void
     {
         self::assertTrue(true);
