b580562de9841c52269771c217f752ed32dbf2d8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:04:36 -0600

    Create Runner.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Runner.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify;
+
+use Generator;
+use Ghostwriter\Testify\Interface\ProjectInterface;
+use Ghostwriter\Testify\Interface\RunnerInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+
+use function str_replace;
+
+final readonly class Runner implements RunnerInterface
+{
+    public function __construct(
+        private ProgressBar $progressBar,
+        private PhpFileFinder $phpFileFinder,
+    ) {
+    }
+
+    public function run(ProjectInterface $project): Generator
+    {
+        $sourceDirectory = $project->source;
+
+        $testsDirectory = $project->tests;
+
+        $this->progressBar->start();
+
+        foreach ($this->phpFileFinder->find($sourceDirectory) as $file) {
+            yield $file => str_replace([$sourceDirectory, '.php'], [$testsDirectory, 'Test.php'], $file);
+
+            $this->progressBar->advance();
+        }
+
+        $this->progressBar->finish();
+    }
+}
