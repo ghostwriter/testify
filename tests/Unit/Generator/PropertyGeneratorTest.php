ba8c3d334ba259ab6fade0ed47ded36d3fc0bbf1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 10:06:34 -0600

    Update PropertyGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/PropertyGeneratorTest.php
+++ tests/Unit/Generator/PropertyGeneratorTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
@@ -13,21 +13,41 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(PropertyGenerator::class)]
 final class PropertyGeneratorTest extends TestCase
 {
-    public static function testConstructDataProvider(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0', 'parameter-1', 'parameter-2', 'parameter-3', 'parameter-4', 'parameter-5', 'parameter-6'],
-        ];
-    }
-
-    #[DataProvider('testConstructDataProvider')]
-    public function testConstruct(string $name, mixed $value, bool $isStatic, bool $isPublic, bool $isProtected, bool $isPrivate, bool $isReadonly): void
+    protected function setUp(): void
     {
         self::markTestSkipped('Not implemented yet.');
+    }
+
+    #[DataProvider('dataProvidertestConstruct')]
+    public function testConstruct(
+        string $name,
+        mixed $value,
+        bool $isStatic,
+        bool $isPublic,
+        bool $isProtected,
+        bool $isPrivate,
+        bool $isReadonly
+    ): void {
+        self::assertTrue(true);
     }
 
     public function testGenerate(): void
     {
-        self::markTestSkipped('Not implemented yet.');
+        self::assertTrue(true);
+    }
+
+    public static function dataProvidertestConstruct(): Generator
+    {
+        yield from [
+            'testConstruct' => [
+                'parameter-0',
+                'parameter-1',
+                'parameter-2',
+                'parameter-3',
+                'parameter-4',
+                'parameter-5',
+                'parameter-6',
+            ],
+        ];
     }
 }
