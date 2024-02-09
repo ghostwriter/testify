8fa9d0a893af547d74b11dcf9a075d40ca04215e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:09:06 -0600

    Create AttributeGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/AttributeGenerator.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator;
+
+use Ghostwriter\Testify\Interface\GeneratorInterface;
+
+use function rtrim;
+
+final readonly class AttributeGenerator implements GeneratorInterface
+{
+    public function __construct(
+        public string $name,
+        public array $params = [],
+    ) {
+    }
+
+    public function generate(): string
+    {
+        $code = '#[';
+
+        $code .= $this->name . '(';
+
+        foreach ($this->params as $param) {
+            $code .= $param . ', ';
+        }
+
+        return rtrim($code, ', ') . ')]';
+    }
+}
