bb5bb009e34a2b3e46fece023e7693f30eb9ce21
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 10:07:15 -0600

    Update StubNotFoundExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/StubNotFoundExceptionTest.php
+++ tests/Unit/Exception/StubNotFoundExceptionTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Exception;
 
+use Generator;
 use Ghostwriter\Testify\Exception\StubNotFoundException;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(StubNotFoundException::class)]
@@ -16,8 +18,61 @@ final class StubNotFoundExceptionTest extends TestCase
         self::markTestSkipped('Not implemented yet.');
     }
 
-    public function testExample(): void
+    #[DataProvider('dataProvidertestConstruct')]
+    public function testConstruct(string $message, int $code, Throwable $previous): void
     {
         self::assertTrue(true);
+    }
+
+    final public function testGetCode(): void
+    {
+        self::assertTrue(true);
+    }
+
+    final public function testGetFile(): void
+    {
+        self::assertTrue(true);
+    }
+
+    final public function testGetLine(): void
+    {
+        self::assertTrue(true);
+    }
+
+    final public function testGetMessage(): void
+    {
+        self::assertTrue(true);
+    }
+
+    final public function testGetPrevious(): void
+    {
+        self::assertTrue(true);
+    }
+
+    final public function testGetTrace(): void
+    {
+        self::assertTrue(true);
+    }
+
+    final public function testGetTraceAsString(): void
+    {
+        self::assertTrue(true);
+    }
+
+    public function testToString(): void
+    {
+        self::assertTrue(true);
+    }
+
+    public function testWakeup(): void
+    {
+        self::assertTrue(true);
+    }
+
+    public static function dataProvidertestConstruct(): Generator
+    {
+        yield from [
+            'testConstruct' => ['parameter-0', 'parameter-1', 'parameter-2'],
+        ];
     }
 }
