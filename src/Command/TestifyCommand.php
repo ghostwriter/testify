46ee01b953ef48a13bc3eb59bd6a49cf6432c79d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:13:16 -0600

    Update TestifyCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/TestifyCommand.php
+++ src/Command/TestifyCommand.php
@@ -6,19 +6,11 @@ namespace Ghostwriter\Testify\Command;
 
 use Composer\InstalledVersions;
 use Ghostwriter\Testify\Filesystem;
-use Ghostwriter\Testify\PhpFileFinder;
+use Ghostwriter\Testify\Interface\PrinterInterface;
+use Ghostwriter\Testify\Interface\RunnerInterface;
 use Ghostwriter\Testify\Project;
 use Ghostwriter\Testify\TestBuilder;
 use Override;
-use Psalm\Config;
-use Psalm\Internal\Analyzer\ProjectAnalyzer;
-use Psalm\Internal\IncludeCollector;
-use Psalm\Internal\Provider\FileProvider;
-use Psalm\Internal\Provider\Providers;
-use Psalm\Progress\VoidProgress;
-use Psalm\Report;
-use Psalm\Report\ReportOptions;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -28,20 +20,16 @@ use Throwable;
 
 use const PHP_EOL;
 
-use function gc_collect_cycles;
-use function memory_get_usage;
-use function round;
 use function sprintf;
-use function str_replace;
-use function vsprintf;
 
 /** @see TestifyTest */
 final class TestifyCommand extends SingleCommandApplication
 {
     public function __construct(
         private readonly Filesystem $filesystem,
+        private readonly RunnerInterface $runner,
+        private readonly PrinterInterface $printer,
         private readonly TestBuilder $testBuilder,
-        private readonly PhpFileFinder $phpFileFinder,
     ) {
         parent::__construct('Testify');
 
@@ -53,109 +41,78 @@ final class TestifyCommand extends SingleCommandApplication
         $this->addArgument('source', InputArgument::OPTIONAL, 'The path to search for missing tests.', 'src');
         $this->addArgument('tests', InputArgument::OPTIONAL, 'The path used to create tests.', 'tests');
         $this->addOption('dry-run', 'd', InputOption::VALUE_NONE, 'Do not write any files.');
+        $this->addOption('force', 'f', InputOption::VALUE_NONE, 'Overwrite existing files.');
     }
 
     #[Override]
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln(sprintf(
-            '<info>%s</info> by <info>%s</info> (<comment>%s</comment>) and contributors. %s' . PHP_EOL,
-            'Testify',
-            'Nathanael Esayeas',
-            'https://ghostwriter.github.io',
-            '<error>#BlackLivesMatter</error>'
-        ));
-
+        $output->writeln(
+            sprintf(
+                '<info>%s</info> by <comment>%s</comment> and contributors. <error>%s</error>' . PHP_EOL,
+                'Testify',
+                'Nathanael Esayeas',
+                '#BlackLivesMatter'
+            )
+        );
         try {
             $project = Project::new($input);
         } catch (Throwable $exception) {
-            $output->writeln([$exception::class, $exception->getMessage(), $exception->getTraceAsString()]);
+            $output->writeln(
+                sprintf(
+                    '<error>%s: %s</error>' . PHP_EOL . PHP_EOL . '%s',
+                    $exception::class,
+                    $exception->getMessage(),
+                    $exception->getTraceAsString()
+                )
+            );
 
             return self::INVALID;
         }
 
         $count = 0;
         $dryRun = $project->dryRun;
-        $sourceDirectory = $project->source;
-        $testsDirectory = $project->tests;
+        $force = $project->force;
+        foreach ($this->runner->run($project) as $file => $testFile) {
+            $output->writeln([
+                'Class <comment>' . $file . '</comment> is missing a test.',
+                'Generating <info>' . $testFile . '</info>.',
+                PHP_EOL,
+            ], OutputInterface::VERBOSITY_VERBOSE);
 
-        $progressBar = new ProgressBar($output);
-        foreach ($progressBar->iterate($this->phpFileFinder->find($sourceDirectory)) as $file) {
-            gc_collect_cycles();
+            $fileGenerator = $this->testBuilder->build($file, $testFile);
 
-            $testFile = str_replace([$sourceDirectory, '.php'], [$testsDirectory, 'Test.php'], $file);
+            $testFileContent = $this->printer->print($fileGenerator);
 
-            if ($dryRun || $this->filesystem->missing($testFile)) {
-                ++$count;
+            $output->writeln(['------', PHP_EOL . $testFileContent . PHP_EOL], OutputInterface::VERBOSITY_VERBOSE);
 
-                $output->writeln([
-                    PHP_EOL,
-                    'Class <comment>' . $file . '</comment> is missing a test.',
-                    'Generating <info>' . $testFile . '</info>.',
-                    self::memoryUsage(),
-                    PHP_EOL,
-                ]);
+            if ($dryRun) {
+                $output->writeln('<info>Dry run, not writing file.</info>');
 
-                $testFileContent = $this->testBuilder->build($file, $testsDirectory);
-
-                $output->writeln(PHP_EOL . $testFileContent . PHP_EOL);
-
-                if ($dryRun) {
-                    $output->writeln('<info>Dry run, not writing file.</info>');
-                    continue;
-                }
-
-                $this->filesystem->save($testFile, $testFileContent);
+                continue;
             }
+
+            if ($force || $this->filesystem->missing($testFile)) {
+                ++$count;
+                $this->filesystem->save($testFile, $testFileContent);
+
+                $output->writeln(
+                    sprintf('<info>File "%s" written.</info>' . PHP_EOL, $testFile),
+                    OutputInterface::VERBOSITY_VERBOSE
+                );
+
+                continue;
+            }
+
+            $output->writeln(
+                '<info>File already exists</info>;<comment>(use "--force|-f" to overwrite)</comment>' . PHP_EOL,
+                OutputInterface::VERBOSITY_VERBOSE
+            );
         }
 
         $output->writeln(
-            sprintf('%s<comment>Found <info>%d</info> missing tests.</comment>', PHP_EOL . PHP_EOL, $count)
+            [PHP_EOL, sprintf('<comment>Generated <info>%d</info> missing tests.</comment>', $count)]
         );
-
         return self::SUCCESS;
-    }
-
-    public static function memoryUsage(): string
-    {
-        $memoryUsage = memory_get_usage(true);
-
-        return vsprintf('<info>%s</info> %s used', match (true) {
-            $memoryUsage < 1024 => [$memoryUsage, 'bytes'],
-            $memoryUsage < 1048576 => [round($memoryUsage / 1024, 2), 'KiB'],
-            $memoryUsage < 1073741824 => [round($memoryUsage / 1048576, 2), 'MiB'],
-            $memoryUsage < 1099511627776 => [round($memoryUsage / 1073741824, 2), 'GiB'],
-            $memoryUsage < 1125899906842624 => [round($memoryUsage / 1099511627776, 2), 'TiB'],
-        });
-    }
-
-    public static function new(): self
-    {
-        $filesystem = new Filesystem();
-
-        $currentWorkingDirectory = $filesystem->currentWorkingDirectory();
-
-        $config = Config::loadFromXML($currentWorkingDirectory, '<psalm/>');
-        $config->cache_directory = null;
-        $config->setIncludeCollector(new IncludeCollector());
-        //        $config->addPluginClass('Ghostwriter\Testify\PsalmPlugin');
-        //        $config->addPluginPath($currentWorkingDirectory . '/vendor/ghostwriter/psalm-plugin/src/Plugin.php');
-
-        $stdout_report_options = new ReportOptions();
-        $stdout_report_options->show_suggestions = false;
-        $stdout_report_options->format = Report::TYPE_JSON;
-
-        return new self(
-            $filesystem,
-            new TestBuilder(new ProjectAnalyzer(
-                $config,
-                new Providers(new FileProvider()),
-                $stdout_report_options,
-                [],
-                1,
-                new VoidProgress(),
-            )),
-            new PhpFileFinder($filesystem),
-        );
     }
 }
