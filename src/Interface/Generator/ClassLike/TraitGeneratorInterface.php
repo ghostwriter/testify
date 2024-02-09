61cb21e476580175a7c5d2f2d4ff446025f523aa
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:06:26 -0600

    Create TraitGeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Generator/ClassLike/TraitGeneratorInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Interface\Generator\ClassLike;
+
+use Ghostwriter\Testify\Interface\Generator\ClassLikeGeneratorInterface;
+
+interface TraitGeneratorInterface extends ClassLikeGeneratorInterface
+{
+}
