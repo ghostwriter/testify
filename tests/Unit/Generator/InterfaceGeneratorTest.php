3c4142134ad2737dba186318294086dffeda3081
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 10:06:46 -0600

    Update InterfaceGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/InterfaceGeneratorTest.php
+++ tests/Unit/Generator/InterfaceGeneratorTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
@@ -13,44 +13,49 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(InterfaceGenerator::class)]
 final class InterfaceGeneratorTest extends TestCase
 {
-    public static function testConstructDataProvider(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0', 'parameter-1', 'parameter-2', 'parameter-3', 'parameter-4'],
-        ];
-    }
-
-    #[DataProvider('testConstructDataProvider')]
-    public function testConstruct(string $name, array $uses, array $extends, array $constants, array $methods): void
+    protected function setUp(): void
     {
         self::markTestSkipped('Not implemented yet.');
     }
 
-    public static function testAddMethodDataProvider(): Generator
+    #[DataProvider('dataProvidertestAddMethod')]
+    public function testAddMethod(GeneratorInterface $method): void
+    {
+        self::assertTrue(true);
+    }
+
+    #[DataProvider('dataProvidertestConstruct')]
+    public function testConstruct(string $name, array $uses, array $extends, array $constants, array $methods): void
+    {
+        self::assertTrue(true);
+    }
+
+    public function testGenerate(): void
+    {
+        self::assertTrue(true);
+    }
+
+    public function testName(): void
+    {
+        self::assertTrue(true);
+    }
+
+    public function testUses(): void
+    {
+        self::assertTrue(true);
+    }
+
+    public static function dataProvidertestAddMethod(): Generator
     {
         yield from [
             'testAddMethod' => ['parameter-0'],
         ];
     }
 
-    #[DataProvider('testAddMethodDataProvider')]
-    public function testAddMethod(GeneratorInterface $method): void
+    public static function dataProvidertestConstruct(): Generator
     {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    public function testGenerate(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    public function testName(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    public function testUses(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
+        yield from [
+            'testConstruct' => ['parameter-0', 'parameter-1', 'parameter-2', 'parameter-3', 'parameter-4'],
+        ];
     }
 }
