4d3354dd0cffcdab475144ecf1bbcf9f48eedebf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 10:06:24 -0600

    Update TestMethodGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/TestMethodGeneratorTest.php
+++ tests/Unit/Generator/TestMethodGeneratorTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
@@ -7,37 +7,65 @@ namespace Ghostwriter\TestifyTests\Unit\Generator;
 use Generator;
 use Ghostwriter\Testify\Generator\TestMethodGenerator;
 use Ghostwriter\Testify\TestMethodNameNormalizer;
+use PhpParser\BuilderFactory;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
-use PhpParser\BuilderFactory;
 
 #[CoversClass(TestMethodGenerator::class)]
 final class TestMethodGeneratorTest extends TestCase
 {
-    public static function testConstructDataProvider(): Generator
+    protected function setUp(): void
+    {
+        self::markTestSkipped('Not implemented yet.');
+    }
+
+    #[DataProvider('dataProvidertestConstruct')]
+    public function testConstruct(
+        BuilderFactory $builderFactory,
+        TestMethodNameNormalizer $testMethodNameNormalizer
+    ): void {
+        self::assertTrue(true);
+    }
+
+    #[DataProvider('dataProvidertestInvoke')]
+    public function testInvoke(
+        string $className,
+        string $methodName,
+        bool $isStatic,
+        bool $isFinal,
+        bool $isAbstract,
+        bool $isPublic,
+        bool $isProtected,
+        bool $isPrivate,
+        array $params,
+        mixed $returnType
+    ): void {
+        self::assertTrue(true);
+    }
+
+    public static function dataProvidertestConstruct(): Generator
     {
         yield from [
             'testConstruct' => ['parameter-0', 'parameter-1'],
         ];
     }
 
-    #[DataProvider('testConstructDataProvider')]
-    public function testConstruct(BuilderFactory $builderFactory, TestMethodNameNormalizer $testMethodNameNormalizer): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    public static function testInvokeDataProvider(): Generator
+    public static function dataProvidertestInvoke(): Generator
     {
         yield from [
-            'testInvoke' => ['parameter-0', 'parameter-1', 'parameter-2', 'parameter-3', 'parameter-4', 'parameter-5', 'parameter-6', 'parameter-7', 'parameter-8', 'parameter-9'],
+            'testInvoke' => [
+                'parameter-0',
+                'parameter-1',
+                'parameter-2',
+                'parameter-3',
+                'parameter-4',
+                'parameter-5',
+                'parameter-6',
+                'parameter-7',
+                'parameter-8',
+                'parameter-9',
+            ],
         ];
-    }
-
-    #[DataProvider('testInvokeDataProvider')]
-    public function testInvoke(string $className, string $methodName, bool $isStatic, bool $isFinal, bool $isAbstract, bool $isPublic, bool $isProtected, bool $isPrivate, array $params, mixed $returnType): void
-    {
-        self::markTestSkipped('Not implemented yet.');
     }
 }
