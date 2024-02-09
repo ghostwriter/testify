00cbdd5e04cdc2c6e71e8b04d2ae5fb18f85f6a1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 11:47:01 -0600

    Move Normalizers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Normalizer/TestDataProviderMethodNameNormalizer.php
+++ src/Normalizer/TestDataProviderMethodNameNormalizer.php
@@ -2,22 +2,21 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Testify;
+namespace Ghostwriter\Testify\Normalizer;
 
+use Ghostwriter\Testify\Formatter\TestDataProviderMethodNameFormatter;
 use Ghostwriter\Testify\Interface\NormalizerInterface;
-
-use function lcfirst;
-use function ltrim;
 
 final readonly class TestDataProviderMethodNameNormalizer implements NormalizerInterface
 {
     public function __construct(
         private TestDataProviderMethodNameFormatter $testDataProviderMethodNameFormatter,
+        private ClassMethodNameNormalizer           $classMethodNormalizer,
     ) {
     }
 
     public function normalize(string $name): string
     {
-        return $this->testDataProviderMethodNameFormatter->format(lcfirst(ltrim($name, '_')));
+        return $this->classMethodNormalizer->normalize($this->testDataProviderMethodNameFormatter->format($name));
     }
 }
---
+++ src/Normalizer/TestMethodNameNormalizer.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Normalizer;
+
+use Ghostwriter\Testify\Formatter\TestMethodNameFormatter;
+use Ghostwriter\Testify\Interface\NormalizerInterface;
+
+final readonly class TestMethodNameNormalizer implements NormalizerInterface
+{
+    public function __construct(
+        private TestMethodNameFormatter   $testMethodNameFormatter,
+        private ClassMethodNameNormalizer $classMethodNormalizer,
+    ) {
+    }
+
+    public function normalize(string $name): string
+    {
+        return $this->classMethodNormalizer->normalize($this->testMethodNameFormatter->format($name));
+    }
+}
