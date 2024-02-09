d68bb8136a1ee4a4d6780ed475804610fee322c9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:08:30 -0600

    Create UseClassGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/UseClassGenerator.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator;
+
+use Ghostwriter\Testify\Interface\Generator\UseClassGeneratorInterface;
+
+final readonly class UseClassGenerator implements UseClassGeneratorInterface
+{
+    use UseGeneratorTrait;
+
+    public function generate(): string
+    {
+        $code = 'use ' . $this->name;
+
+        if ($this->alias !== '') {
+            $code .= ' as ' . $this->alias;
+        }
+
+        return $code . ';';
+    }
+}
