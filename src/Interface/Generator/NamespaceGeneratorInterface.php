187b71a1708cb767536920dc958d67c09bdf3556
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:06:00 -0600

    Create NamespaceGeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Generator/NamespaceGeneratorInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Interface\Generator;
+
+use Ghostwriter\Testify\Interface\GeneratorInterface;
+
+interface NamespaceGeneratorInterface extends GeneratorInterface
+{
+    public function name(): string;
+}
