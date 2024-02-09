aef6a17763094084715183713e78fb7e28adda40
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:08:41 -0600

    Create TestDataProviderGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/TestDataProviderGenerator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator;
+
+use Ghostwriter\Testify\Interface\GeneratorInterface;
+
+use function implode;
+use function sprintf;
+
+final readonly class TestDataProviderGenerator implements GeneratorInterface
+{
+    /**
+     * @param array<ParameterGenerator> $parameters
+     */
+    public function __construct(
+        private string $name,
+        private array $parameters
+    ) {
+    }
+
+    public function generate(): string
+    {
+        $data = [];
+        $position = 0;
+        foreach ($this->parameters as $parameter) {
+            $data[$parameter->name()] = sprintf("'parameter-%d'", $position++);
+        }
+
+        $lines = ['yield from ['];
+        $lines[] = sprintf(
+            "%s'%s' => [%s],",
+            self::INDENT . self::INDENT . self::INDENT,
+            $this->name,
+            implode(', ', $data)
+        );
+
+        $lines[] = self::INDENT . self::INDENT . '];';
+
+        return implode(self::NEWLINE, $lines);
+    }
+}
