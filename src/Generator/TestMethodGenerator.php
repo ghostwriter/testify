3947b3f933c42b480b2cd502ff1c14d36a1f5b81
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:08:37 -0600

    Create TestMethodGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/TestMethodGenerator.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator;
+
+use Ghostwriter\Testify\TestMethodNameNormalizer;
+use PhpParser\BuilderFactory;
+use PhpParser\Node;
+
+final readonly class TestMethodGenerator
+{
+    public function __construct(
+        private BuilderFactory $builderFactory = new BuilderFactory(),
+        private TestMethodNameNormalizer $testMethodNameNormalizer = new TestMethodNameNormalizer(),
+    ) {
+    }
+
+    public function __invoke(
+        string $className,
+        string $methodName,
+        bool $isStatic,
+        bool $isFinal,
+        bool $isAbstract,
+        bool $isPublic,
+        bool $isProtected,
+        bool $isPrivate,
+        array $params,
+        mixed $returnType,
+    ): Node {
+        $method = $this->builderFactory->method($this->testMethodNameNormalizer->normalize($methodName));
+        if ($isStatic) {
+            $method->makeStatic();
+        }
+
+        if ($isFinal) {
+            $method->makeFinal();
+        }
+
+        if ($isAbstract) {
+            $method->makeAbstract();
+        }
+
+        if ($isPublic) {
+            $method->makePublic();
+        }
+
+        if ($isProtected) {
+            $method->makeProtected();
+        }
+
+        if ($isPrivate) {
+            $method->makePrivate();
+        }
+
+        if ($returnType !== null) {
+            $method->setReturnType($returnType);
+        }
+
+        foreach ($params as $param) {
+            $method->addParam(...$param);
+        }
+
+        return $method->getNode();
+    }
+}
