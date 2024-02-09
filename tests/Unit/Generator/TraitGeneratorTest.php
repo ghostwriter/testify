e89ea5dd9dbc251a5f1561843a022a93f2351a8a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 10:06:28 -0600

    Update TraitGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/TraitGeneratorTest.php
+++ tests/Unit/Generator/TraitGeneratorTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
@@ -13,31 +13,49 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(TraitGenerator::class)]
 final class TraitGeneratorTest extends TestCase
 {
-    public static function testConstructDataProvider(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0', 'parameter-1', 'parameter-2', 'parameter-3', 'parameter-4', 'parameter-5'],
-        ];
-    }
-
-    #[DataProvider('testConstructDataProvider')]
-    public function testConstruct(string $name, array $uses, array $constants, array $properties, array $methods, array $traitUses): void
+    protected function setUp(): void
     {
         self::markTestSkipped('Not implemented yet.');
+    }
+
+    #[DataProvider('dataProvidertestConstruct')]
+    public function testConstruct(
+        string $name,
+        array $uses,
+        array $constants,
+        array $properties,
+        array $methods,
+        array $traitUses
+    ): void {
+        self::assertTrue(true);
     }
 
     public function testGenerate(): void
     {
-        self::markTestSkipped('Not implemented yet.');
+        self::assertTrue(true);
     }
 
     public function testName(): void
     {
-        self::markTestSkipped('Not implemented yet.');
+        self::assertTrue(true);
     }
 
     public function testUses(): void
     {
-        self::markTestSkipped('Not implemented yet.');
+        self::assertTrue(true);
+    }
+
+    public static function dataProvidertestConstruct(): Generator
+    {
+        yield from [
+            'testConstruct' => [
+                'parameter-0',
+                'parameter-1',
+                'parameter-2',
+                'parameter-3',
+                'parameter-4',
+                'parameter-5',
+            ],
+        ];
     }
 }
