9d464b6cf2b2937903148d26cf8fef7092e1b24d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:08:34 -0600

    Create TraitGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/TraitGenerator.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator;
+
+use Ghostwriter\Testify\Interface\Generator\ClassLike\TraitGeneratorInterface;
+use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\ConstantGeneratorInterface;
+use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\MethodGeneratorInterface;
+use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\PropertyGeneratorInterface;
+use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\TraitUseGeneratorInterface;
+
+final class TraitGenerator implements TraitGeneratorInterface
+{
+    /**
+     * @param array<ConstantGeneratorInterface> $constants
+     * @param array<PropertyGeneratorInterface> $properties
+     * @param array<MethodGeneratorInterface>   $methods
+     * @param array<TraitUseGeneratorInterface> $traitUses
+     */
+    public function __construct(
+        private string $name,
+        private array $uses = [],
+        private array $constants = [],
+        private array $properties = [],
+        private array $methods = [],
+        private array $traitUses = [],
+    ) {
+    }
+
+    public function generate(): string
+    {
+        $code = 'trait ' . $this->name . ' {' . self::NEWLINES;
+
+        foreach ($this->traitUses as $traitUse) {
+            $code .= $traitUse->generate() . self::NEWLINE;
+        }
+
+        foreach ($this->constants as $constant) {
+            $code .= $constant->generate() . self::NEWLINE;
+        }
+
+        foreach ($this->properties as $property) {
+            $code .= $property->generate() . self::NEWLINE;
+        }
+
+        foreach ($this->methods as $method) {
+            $code .= $method->generate() . self::NEWLINE;
+        }
+
+        return $code . '}' . self::NEWLINE;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function uses(): array
+    {
+        return $this->uses;
+    }
+}
