60f2f5305a1135ff508e87346b313fef9f83ccb2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:04:29 -0600

    Update TestBuilder.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/TestBuilder.php
+++ src/TestBuilder.php
@@ -4,304 +4,58 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify;
 
-use Generator;
-use Ghostwriter\Testify\Exception\NoNamespaceFoundException;
-use Ghostwriter\Testify\NodeVisitor\ChangeToShortArrayNodeVisitor;
-use Ghostwriter\Testify\NodeVisitor\HashNodeVisitor;
-use Ghostwriter\Testify\NodeVisitor\ImportFullyQualifiedNamesNodeVisitor;
-use Ghostwriter\Testify\NodeVisitor\SortClassLikeStatementsAlphabeticallyNodeVisitor;
-use Ghostwriter\Testify\NodeVisitor\SortMatchExpressionsAlphabeticallyNodeVisitor;
-use Ghostwriter\Testify\NodeVisitor\SortUseStatementsAlphabeticallyNodeVisitor;
-use PhpParser\BuilderFactory;
-use PhpParser\Node\Expr\StaticCall;
-use PhpParser\Node\Expr\YieldFrom;
-use PhpParser\Node\Scalar\LNumber;
-use PhpParser\Node\Stmt\Declare_;
-use PhpParser\Node\Stmt\DeclareDeclare;
-use PhpParser\Node\Stmt\Nop;
-use PhpParser\NodeTraverser;
-use PhpParser\NodeVisitor\NodeConnectingVisitor;
-use PhpParser\PrettyPrinter\Standard;
+use Ghostwriter\Testify\Generator\AttributeGenerator;
+use Ghostwriter\Testify\Generator\ClassGenerator;
+use Ghostwriter\Testify\Generator\FileGenerator;
+use Ghostwriter\Testify\Interface\BuilderInterface;
+use Ghostwriter\Testify\Interface\GeneratorInterface;
+use PhpToken;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
-use Psalm\Aliases;
-use Psalm\Internal\Analyzer\ProjectAnalyzer;
 use RuntimeException;
 
-use function array_key_exists;
-use function array_key_first;
-use function array_key_last;
-use function array_unique;
 use function basename;
-use function count;
-use function explode;
-use function implode;
-use function ltrim;
-use function mb_strpos;
-use function mb_strrpos;
-use function mb_substr;
-use function str_contains;
-use function ucfirst;
+use function file_get_contents;
 
-final readonly class TestBuilder
+final readonly class TestBuilder implements BuilderInterface
 {
-    private StaticCall $assertTrueStmt;
-
-    private Declare_ $declareStrictTypes;
-
-    private Nop $nop;
-
-    private YieldFrom $yieldFromStmt;
-
     public function __construct(
-        private ProjectAnalyzer $projectAnalyzer,
-        private BuilderFactory $builderFactory = new BuilderFactory(),
-        private Standard $printer = new Standard(),
-        private NodeTraverser $traverser = new NodeTraverser(),
+        private TestMethodsResolver $testMethodsResolver,
+        private FileResolver $fileResolver,
     ) {
-        $this->assertTrueStmt = $this->builderFactory->staticCall(
-            'self',
-            'assertTrue',
-            [$this->builderFactory->val(true)]
-        );
-
-        $array = $this->builderFactory->val([
-            'example' => [true],
-        ]);
-
-        $this->yieldFromStmt = new YieldFrom($array);
-
-        $this->declareStrictTypes = new Declare_([new DeclareDeclare('strict_types', new LNumber(1))]);
-
-        $this->nop = new Nop();
-
-        $this->traverser->addVisitor(new HashNodeVisitor());
-        $this->traverser->addVisitor(new NodeConnectingVisitor());
-        $this->traverser->addVisitor(new ImportFullyQualifiedNamesNodeVisitor());
-        $this->traverser->addVisitor(new ChangeToShortArrayNodeVisitor());
-        $this->traverser->addVisitor(new SortUseStatementsAlphabeticallyNodeVisitor());
-        $this->traverser->addVisitor(new SortClassLikeStatementsAlphabeticallyNodeVisitor());
-        $this->traverser->addVisitor(new SortMatchExpressionsAlphabeticallyNodeVisitor());
     }
 
-    public function build(string $file, string $testsDirectory): string
+    public function build(string $file, string $testFile): GeneratorInterface
     {
-        $imports = [];
+        $code = file_get_contents($file);
 
-        foreach ($this->check($file) as $namespace => $classes) {
-            $namespaces = explode('\\', $namespace);
-            $namespaces[1] .= 'Tests\\' . basename($testsDirectory, '.php');
-
-            $testNamespace = implode('\\', $namespaces);
-
-            $namespaceBuilder = $this->builderFactory->namespace($testNamespace);
-
-            foreach ($classes as $class => $classInfo) {
-                $classBuilder = $this->builderFactory->class($class . 'Test')
-                    ->extend('TestCase')
-                    ->makeFinal()
-                    ->addAttribute(
-                        $this->builderFactory->attribute('CoversClass', [
-                            $this->builderFactory ->classConstFetch($class, 'class'),
-                        ])
-                    );
-
-                $classBuilder->addStmt(
-                    $this->builderFactory->method('setUp')
-                        ->makeProtected()
-                        ->setReturnType('void')
-                        ->addStmt(
-                            $this->builderFactory->staticCall(
-                                'self',
-                                'markTestSkipped',
-                                [$this->builderFactory->val('Not implemented yet.')]
-                            )
-                        )
-                );
-
-                $methods = $classInfo['Methods'];
-
-                if ($methods === []) {
-                    $classBuilder->addStmt(
-                        $this->builderFactory
-                            ->method('testExample')
-                            ->makePublic()
-                            ->setReturnType('void')
-                            ->addStmt($this->assertTrueStmt)
-                    );
-                }
-
-                foreach ($methods as $method => $methodInfo) {
-                    $methodBuilder = $this->builderFactory->method($method)
-                        ->makePublic()
-                        ->setReturnType($methodInfo['return'])
-                        ->addStmt($this->assertTrueStmt);
-
-                    foreach ($methodInfo['Params'] ?? [] as $param => $type) {
-                        $methodBuilder->addParam($this->builderFactory->param($param)->setType($type));
-                    }
-
-                    if (array_key_exists('dataProvider', $methodInfo)) {
-                        $dataProviderName = $methodInfo['dataProvider'];
-
-                        $classBuilder->addStmt(
-                            $this->builderFactory
-                                ->method($dataProviderName)
-                                ->makePublic()
-                                ->makeStatic()
-                                ->setReturnType(Generator::class)
-                                ->addStmt($this->yieldFromStmt)
-                        );
-
-                        $methodBuilder->addAttribute(
-                            $this->builderFactory ->attribute('DataProvider', [$dataProviderName])
-                        );
-                    }
-
-                    $classBuilder->addStmt($methodBuilder);
-                }
-
-                $methodImports = array_unique($classInfo['Imports'] ?? []);
-
-                foreach ($methodImports as $use) {
-                    if (array_key_exists($use, $imports)) {
-                        continue;
-                    }
-
-                    //                    $parts = explode('\\', $use);
-                    //                    $partsCount = count($parts);
-                    //
-                    //                    if ($partsCount > 1) {
-                    //                        $alias = ucfirst($parts[array_key_first($parts)])
-                    //                            . ucfirst($parts[array_key_last($parts)]);
-                    //
-                    //                        if (! array_key_exists($alias, $imports)) {
-                    //                            $imports[$alias] = $use;
-                    //                            $namespaceBuilder->addStmt($this->builderFactory->use($use) ->as($alias));
-                    //
-                    //                            continue;
-                    //                        }
-                    //                    }
-
-                    $imports[$use] = $use;
-                    $namespaceBuilder->addStmt($this->builderFactory->use($use));
-                }
-
-                $namespaceBuilder->addStmt($this->builderFactory->use($namespace . '\\' . $class));
-                $namespaceBuilder->addStmt($this->nop);
-                $namespaceBuilder->addStmt($classBuilder);
-            }
-
-            return $this->printer->prettyPrintFile(
-                $this->traverser->traverse([
-                    $this->declareStrictTypes,
-                    $this->nop,
-                    $namespaceBuilder->getNode(),
-                    $this->nop,
-                ])
-            );
+        if ($code === false) {
+            throw new RuntimeException('Could not read file: ' . $file);
         }
 
-        return '';
-    }
+        $tokens = PhpToken::tokenize($code);
 
-    private function check(string $file): array
-    {
-        $this->projectAnalyzer->checkFile($file);
+        $namespaces = $this->fileResolver->resolve($tokens);
 
-        $namespace = null;
-        $codebase = $this->projectAnalyzer->getCodebase();
-        foreach ($codebase->file_storage_provider->get($file)->namespace_aliases as $aliases) {
-            if (! $aliases instanceof Aliases) {
-                continue;
-            }
-
-            $namespace = $aliases->namespace;
-            if ($namespace === null) {
-                continue;
-            }
-
-            break;
+        $class = basename($file, '.php');
+        $testClass = basename($testFile, '.php');
+        foreach ($namespaces as $namespace => [$testNamespace, $namespaceGenerator]) {
+            $namespaceClass = $namespace . '\\' . $class;
+            $testNamespaceClass = $testNamespace . '\\' . $testClass;
+            $namespaces[$namespace] = $namespaceGenerator->classLikes([
+                $testNamespaceClass => new ClassGenerator(
+                    name: $testClass,
+                    extends: 'TestCase',
+                    methods: $this->testMethodsResolver->resolve($namespace . '\\' . $class),
+                    attributes: [new AttributeGenerator('CoversClass', [$class . '::class'])],
+                    isFinal: true
+                ),
+            ])
+                ->usesClass(TestCase::class)
+                ->usesClass(CoversClass::class)
+                ->usesClass($namespaceClass);
         }
 
-        if ($namespace === null) {
-            throw new NoNamespaceFoundException($file);
-        }
-
-        $classes = [];
-        foreach ($codebase->classlike_storage_provider->getAll() as $classLikeStorage) {
-            $location = $classLikeStorage->location;
-            if ($location === null) {
-                continue;
-            }
-
-            if ($location->file_path !== $file) {
-                continue;
-            }
-
-            $fullyQualifiedClassName = $classLikeStorage->name;
-            $lastSlash = mb_strrpos($fullyQualifiedClassName, '\\');
-            if ($lastSlash === false) {
-                continue;
-            }
-
-            $className = mb_substr($fullyQualifiedClassName, $lastSlash + 1);
-
-            $imports = [CoversClass::class, TestCase::class];
-            $methods = [];
-            foreach ($classLikeStorage->methods as $methodStorage) {
-                $methodName = $methodStorage->cased_name ?? throw new RuntimeException('No cased name found.');
-
-                $casedName = ucfirst(ltrim($methodName, '_'));
-                $dataProvider = 'dataProvider' . $casedName;
-                $testMethodName = 'test' . $casedName;
-
-                $methods[$testMethodName] = [
-                    'Params' => [],
-                    'return' => 'void',
-                ];
-
-                $params = [];
-                foreach ($methodStorage->params as $methodParam) {
-                    $type = $methodParam->type?->getId() ?? throw new RuntimeException('No type found.');
-
-                    if (str_contains($type, '\\')) {
-                        $imports[] = $type;
-                    }
-
-                    $position = mb_strpos($type, '<');
-                    if ($position !== false) {
-                        $type = mb_substr($type, 0, $position);
-
-                        $params[$methodParam->name] = $type;
-
-                        continue;
-                    }
-
-                    $position = mb_strpos($type, '(');
-                    if ($position !== false) {
-                        $type = mb_substr($type, 0, $position);
-
-                        $params[$methodParam->name] = $type;
-
-                        continue;
-                    }
-
-                    $params[$methodParam->name] = $type;
-                }
-
-                if ($params !== []) {
-                    $methods[$testMethodName]['Params'] = $params;
-                    $methods[$testMethodName]['dataProvider'] = $dataProvider;
-                    $imports[] = DataProvider::class;
-                    $imports[] = Generator::class;
-                }
-            }
-            $classes[$namespace][$className]['Imports'] = array_unique($imports);
-            $classes[$namespace][$className]['Methods'] = $methods;
-        }
-
-        return $classes;
+        return FileGenerator::new($namespaces)->declareStrictTypes();
     }
 }
