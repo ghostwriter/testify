ff76f08b685d921ef97535c1683e0e8d05b55d0a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:08:54 -0600

    Create InterfaceNameGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/InterfaceNameGenerator.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator;
+
+use Ghostwriter\Testify\Interface\Generator\NameGeneratorInterface;
+
+final readonly class InterfaceNameGenerator implements NameGeneratorInterface
+{
+    public function __construct(
+        private string $name
+    ) {
+    }
+
+    public function generate(): string
+    {
+        return $this->name;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+}
