1b0b29e6f63228fdf369bb26c8a87e76d7234141
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:06:17 -0600

    Create PropertyGeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Generator/ClassLikeMember/PropertyGeneratorInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Interface\Generator\ClassLikeMember;
+
+use Ghostwriter\Testify\Interface\Generator\ClassLikeMemberGeneratorInterface;
+
+interface PropertyGeneratorInterface extends ClassLikeMemberGeneratorInterface
+{
+}
