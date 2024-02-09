e8c1174add6086e874b6b3fe33cd34a1adc0e4e7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 11:50:52 -0600

    Update NamespaceGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/NamespaceGeneratorTest.php
+++ tests/Unit/Generator/NamespaceGeneratorTest.php
@@ -18,19 +18,19 @@ final class NamespaceGeneratorTest extends TestCase
         self::markTestSkipped('Not implemented yet.');
     }
 
-    #[DataProvider('dataProvidertestClass')]
+    #[DataProvider('dataProviderTestClass')]
     public function testClass(string $name, string $extends, array $methods, array $attributes, bool $isFinal): void
     {
         self::assertTrue(true);
     }
 
-    #[DataProvider('dataProvidertestClassLikes')]
+    #[DataProvider('dataProviderTestClassLikes')]
     public function testClassLikes(array $classLikes): void
     {
         self::assertTrue(true);
     }
 
-    #[DataProvider('dataProvidertestConstruct')]
+    #[DataProvider('dataProviderTestConstruct')]
     public function testConstruct(string $name, array $uses, array $classLikes): void
     {
         self::assertTrue(true);
@@ -51,60 +51,60 @@ final class NamespaceGeneratorTest extends TestCase
         self::assertTrue(true);
     }
 
-    #[DataProvider('dataProvidertestUsesClass')]
+    #[DataProvider('dataProviderTestUsesClass')]
     public function testUsesClass(string $class): void
     {
         self::assertTrue(true);
     }
 
-    #[DataProvider('dataProvidertestUsesConstant')]
+    #[DataProvider('dataProviderTestUsesConstant')]
     public function testUsesConstant(string $constant): void
     {
         self::assertTrue(true);
     }
 
-    #[DataProvider('dataProvidertestUsesFunction')]
+    #[DataProvider('dataProviderTestUsesFunction')]
     public function testUsesFunction(string $function): void
     {
         self::assertTrue(true);
     }
 
-    public static function dataProvidertestClass(): Generator
+    public static function dataProviderTestClass(): Generator
     {
         yield from [
             'testClass' => ['parameter-0', 'parameter-1', 'parameter-2', 'parameter-3', 'parameter-4'],
         ];
     }
 
-    public static function dataProvidertestClassLikes(): Generator
+    public static function dataProviderTestClassLikes(): Generator
     {
         yield from [
             'testClassLikes' => ['parameter-0'],
         ];
     }
 
-    public static function dataProvidertestConstruct(): Generator
+    public static function dataProviderTestConstruct(): Generator
     {
         yield from [
             'testConstruct' => ['parameter-0', 'parameter-1', 'parameter-2'],
         ];
     }
 
-    public static function dataProvidertestUsesClass(): Generator
+    public static function dataProviderTestUsesClass(): Generator
     {
         yield from [
             'testUsesClass' => ['parameter-0'],
         ];
     }
 
-    public static function dataProvidertestUsesConstant(): Generator
+    public static function dataProviderTestUsesConstant(): Generator
     {
         yield from [
             'testUsesConstant' => ['parameter-0'],
         ];
     }
 
-    public static function dataProvidertestUsesFunction(): Generator
+    public static function dataProviderTestUsesFunction(): Generator
     {
         yield from [
             'testUsesFunction' => ['parameter-0'],
