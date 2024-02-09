51c3327e72d200d086582ba5288ad83d885d7596
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 07:08:45 -0600

    Create PropertyGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/PropertyGenerator.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator;
+
+use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\PropertyGeneratorInterface;
+
+final readonly class PropertyGenerator implements PropertyGeneratorInterface
+{
+    public function __construct(
+        private string $name,
+        private mixed $value,
+        private bool $isStatic = false,
+        private bool $isPublic = true,
+        private bool $isProtected = false,
+        private bool $isPrivate = false,
+        private bool $isReadonly = false,
+    ) {
+    }
+
+    public function generate(): string
+    {
+        $code = '';
+
+        if ($this->isStatic) {
+            $code .= 'static ';
+        }
+
+        if ($this->isPublic) {
+            $code .= 'public ';
+        }
+
+        if ($this->isProtected) {
+            $code .= 'protected ';
+        }
+
+        if ($this->isPrivate) {
+            $code .= 'private ';
+        }
+
+        if ($this->isReadonly) {
+            $code .= 'readonly ';
+        }
+
+        $code .= '$' . $this->name;
+
+        if ($this->value !== null) {
+            $code .= ' = ' . $this->value;
+        }
+
+        return $code;
+    }
+}
