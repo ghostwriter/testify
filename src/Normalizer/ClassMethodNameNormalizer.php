0daf5f63a63125aca3d692caf2f396caff2fcba7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-10 14:18:14 -0600

    Update ClassMethodNameNormalizer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Normalizer/ClassMethodNameNormalizer.php
+++ src/Normalizer/ClassMethodNameNormalizer.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Normalizer;
 
+use Ghostwriter\CaseConverter\CaseConverter;
 use Ghostwriter\Testify\Interface\NormalizerInterface;
-
-use function lcfirst;
-use function str_replace;
-use function ucwords;
 
 final readonly class ClassMethodNameNormalizer implements NormalizerInterface
 {
+    public function __construct(
+        private CaseConverter $caseConverter,
+    ) {
+    }
     public function normalize(string $name): string
     {
-        return lcfirst(str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $name))));
+        return $this->caseConverter->camelCase($name);
     }
 }
