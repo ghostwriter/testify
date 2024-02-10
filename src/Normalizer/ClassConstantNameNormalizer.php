ba9d42dff56761a6c5467e6f55bf1ca10b75462c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-10 14:18:05 -0600

    Update ClassConstantNameNormalizer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Normalizer/ClassConstantNameNormalizer.php
+++ src/Normalizer/ClassConstantNameNormalizer.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Normalizer;
 
+use Ghostwriter\CaseConverter\CaseConverter;
 use Ghostwriter\Testify\Interface\NormalizerInterface;
-
-use function mb_strtoupper;
-use function str_replace;
 
 final readonly class ClassConstantNameNormalizer implements NormalizerInterface
 {
+    public function __construct(
+        private CaseConverter $caseConverter,
+    ) {
+    }
     public function normalize(string $name): string
     {
-        return mb_strtoupper(str_replace(['-', '_'], ' ', $name));
+        return $this->caseConverter->macroCase($name);
     }
 }
