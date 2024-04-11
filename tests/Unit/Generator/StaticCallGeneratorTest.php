3dda466a055780a41307de602a638b16a41502cb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:27 -0500

    Update StaticCallGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/StaticCallGeneratorTest.php
+++ tests/Unit/Generator/StaticCallGeneratorTest.php
@@ -4,35 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Generator;
 
-use Generator;
 use Ghostwriter\Testify\Generator\StaticCallGenerator;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(StaticCallGenerator::class)]
 final class StaticCallGeneratorTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(string $class, string $method, array $args): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public function testGenerate(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0', 'parameter-1', 'parameter-2'],
-        ];
     }
 }
