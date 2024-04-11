dd3948b8924d91885078908b3432c9beeea27218
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:25 -0500

    Update TestBuilderTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/TestBuilderTest.php
+++ tests/Unit/TestBuilderTest.php
@@ -4,49 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit;
 
-use Generator;
-use Ghostwriter\Testify\FileResolver;
-use Ghostwriter\Testify\Normalizer\ClassNameNormalizer;
 use Ghostwriter\Testify\TestBuilder;
-use Ghostwriter\Testify\TestMethodsResolver;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(TestBuilder::class)]
 final class TestBuilderTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestBuild')]
-    public function testBuild(string $file, string $testFile): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(
-        TestMethodsResolver $testMethodsResolver,
-        FileResolver $fileResolver,
-        ClassNameNormalizer $classNameNormalizer
-    ): void {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestBuild(): Generator
-    {
-        yield from [
-            'testBuild' => ['parameter-0', 'parameter-1'],
-        ];
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0', 'parameter-1', 'parameter-2'],
-        ];
     }
 }
