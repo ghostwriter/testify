670d42c7fefe04d77002bf59062decf3db778f3f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:25 -0500

    Update ProjectTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -4,30 +4,14 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit;
 
-use Generator;
 use Ghostwriter\Testify\Project;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Console\Input\InputInterface;
 
 #[CoversClass(Project::class)]
 final class ProjectTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    public static function dataProviderTestNew(): Generator
-    {
-        yield from [
-            'testNew' => ['parameter-0'],
-        ];
-    }
-
-    #[DataProvider('dataProviderTestNew')]
-    public static function testNew(InputInterface $input): void
+    public function testExample(): void
     {
         self::assertTrue(true);
     }
