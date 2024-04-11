64fccd7838223a5b932741f10d1364d22cabc92b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:33 -0500

    Update PathDoesNotExistException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/PathDoesNotExistException.php
+++ src/Exception/PathDoesNotExistException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Testify\Exception;
 
 use RuntimeException;
 
-final class PathDoesNotExistException extends RuntimeException
-{
-}
+final class PathDoesNotExistException extends RuntimeException {}
