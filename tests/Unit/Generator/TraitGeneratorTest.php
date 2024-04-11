8f24612cd713edd7d6600b47d9b2ddaaab2de4c7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:26 -0500

    Update TraitGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/TraitGeneratorTest.php
+++ tests/Unit/Generator/TraitGeneratorTest.php
@@ -4,72 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Generator;
 
-use Generator;
 use Ghostwriter\Testify\Generator\TraitGenerator;
-use Ghostwriter\Testify\Interface\Generator\ClassLikeGeneratorInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(TraitGenerator::class)]
 final class TraitGeneratorTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestCompare')]
-    public function testCompare(ClassLikeGeneratorInterface $other): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(
-        string $name,
-        array $uses,
-        array $constants,
-        array $properties,
-        array $methods,
-        array $traitUses
-    ): void {
-        self::assertTrue(true);
-    }
-
-    public function testGenerate(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testName(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testUses(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestCompare(): Generator
-    {
-        yield from [
-            'testCompare' => ['parameter-0'],
-        ];
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => [
-                'parameter-0',
-                'parameter-1',
-                'parameter-2',
-                'parameter-3',
-                'parameter-4',
-                'parameter-5',
-            ],
-        ];
     }
 }
