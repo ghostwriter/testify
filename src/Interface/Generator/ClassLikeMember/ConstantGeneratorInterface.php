06c491def97d9e6b01bdff517e6d78d3abf3593c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:31 -0500

    Update ConstantGeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/Generator/ClassLikeMember/ConstantGeneratorInterface.php
+++ src/Interface/Generator/ClassLikeMember/ConstantGeneratorInterface.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Testify\Interface\Generator\ClassLikeMember;
 
 use Ghostwriter\Testify\Interface\Generator\ClassLikeMemberGeneratorInterface;
 
-interface ConstantGeneratorInterface extends ClassLikeMemberGeneratorInterface
-{
-}
+interface ConstantGeneratorInterface extends ClassLikeMemberGeneratorInterface {}
