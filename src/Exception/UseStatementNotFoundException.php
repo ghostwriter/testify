c69685150a73fdfc3317a75fa3e1d9c2d90a5a90
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:33 -0500

    Update UseStatementNotFoundException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/UseStatementNotFoundException.php
+++ src/Exception/UseStatementNotFoundException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Testify\Exception;
 
 use RuntimeException;
 
-final class UseStatementNotFoundException extends RuntimeException
-{
-}
+final class UseStatementNotFoundException extends RuntimeException {}
