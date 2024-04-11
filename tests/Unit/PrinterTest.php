b7f585b9ea08c5f1b9447d5589cfe515c146f79a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:25 -0500

    Update PrinterTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/PrinterTest.php
+++ tests/Unit/PrinterTest.php
@@ -4,31 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit;
 
-use Generator;
-use Ghostwriter\Testify\Interface\GeneratorInterface;
 use Ghostwriter\Testify\Printer;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(Printer::class)]
 final class PrinterTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestPrint')]
-    public function testPrint(GeneratorInterface $generator): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public static function dataProviderTestPrint(): Generator
-    {
-        yield from [
-            'testPrint' => ['parameter-0'],
-        ];
     }
 }
