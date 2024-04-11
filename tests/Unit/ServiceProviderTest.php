7d4a058083ee11734cee5de30dece5ff57b53f3b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:25 -0500

    Update ServiceProviderTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/ServiceProviderTest.php
+++ tests/Unit/ServiceProviderTest.php
@@ -4,31 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit;
 
-use Generator;
-use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Testify\ServiceProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(ServiceProvider::class)]
 final class ServiceProviderTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestInvoke')]
-    public function testInvoke(ContainerInterface $container): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public static function dataProviderTestInvoke(): Generator
-    {
-        yield from [
-            'testInvoke' => ['parameter-0'],
-        ];
     }
 }
