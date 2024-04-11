f2195f98440aedfa78b05d7be2452518ef2769a6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:28 -0500

    Update TestMethodNameFormatterTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Formatter/TestMethodNameFormatterTest.php
+++ tests/Unit/Formatter/TestMethodNameFormatterTest.php
@@ -4,30 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Formatter;
 
-use Generator;
 use Ghostwriter\Testify\Formatter\TestMethodNameFormatter;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(TestMethodNameFormatter::class)]
 final class TestMethodNameFormatterTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestFormat')]
-    public function testFormat(string $name): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public static function dataProviderTestFormat(): Generator
-    {
-        yield from [
-            'testFormat' => ['parameter-0'],
-        ];
     }
 }
