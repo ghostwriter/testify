24c7a4d739eee62cf4752c07b564a20b20fbe898
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:26 -0500

    Update UseClassGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/UseClassGeneratorTest.php
+++ tests/Unit/Generator/UseClassGeneratorTest.php
@@ -4,59 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Generator;
 
-use Generator;
 use Ghostwriter\Testify\Generator\UseClassGenerator;
-use Ghostwriter\Testify\Interface\Generator\UseGeneratorInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(UseClassGenerator::class)]
 final class UseClassGeneratorTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    final public function testAlias(): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    #[DataProvider('dataProviderTestCompare')]
-    final public function testCompare(UseGeneratorInterface $generator): void
-    {
-        self::assertTrue(true);
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(string $name, string $alias): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testGenerate(): void
-    {
-        self::assertTrue(true);
-    }
-
-    final public function testName(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestCompare(): Generator
-    {
-        yield from [
-            'testCompare' => ['parameter-0'],
-        ];
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0', 'parameter-1'],
-        ];
     }
 }
