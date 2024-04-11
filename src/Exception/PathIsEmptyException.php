b112135eb43c18dbba4dd20c1abf80cbef4ececc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:33 -0500

    Update PathIsEmptyException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/PathIsEmptyException.php
+++ src/Exception/PathIsEmptyException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Testify\Exception;
 
 use RuntimeException;
 
-final class PathIsEmptyException extends RuntimeException
-{
-}
+final class PathIsEmptyException extends RuntimeException {}
