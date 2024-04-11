6f009971e7ea2440b4ef70281c54fb63169cfb88
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:24 -0500

    Update TestMethodsResolverTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/TestMethodsResolverTest.php
+++ tests/Unit/TestMethodsResolverTest.php
@@ -4,47 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit;
 
-use Generator;
-use Ghostwriter\Testify\Normalizer\TestDataProviderMethodNameNormalizer;
-use Ghostwriter\Testify\Normalizer\TestMethodNameNormalizer;
 use Ghostwriter\Testify\TestMethodsResolver;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(TestMethodsResolver::class)]
 final class TestMethodsResolverTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(
-        TestMethodNameNormalizer $testMethodNameNormalizer,
-        TestDataProviderMethodNameNormalizer $testDataProviderMethodNameNormalizer
-    ): void {
-        self::assertTrue(true);
-    }
-
-    #[DataProvider('dataProviderTestResolve')]
-    public function testResolve(string $class): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0', 'parameter-1'],
-        ];
-    }
-
-    public static function dataProviderTestResolve(): Generator
-    {
-        yield from [
-            'testResolve' => ['parameter-0'],
-        ];
     }
 }
