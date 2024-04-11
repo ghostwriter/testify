73de734fdfdc85beb06c78bf6699c71359ba7466
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:27 -0500

    Update InterfaceNameGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/InterfaceNameGeneratorTest.php
+++ tests/Unit/Generator/InterfaceNameGeneratorTest.php
@@ -4,40 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Generator;
 
-use Generator;
 use Ghostwriter\Testify\Generator\InterfaceNameGenerator;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(InterfaceNameGenerator::class)]
 final class InterfaceNameGeneratorTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(string $name): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public function testGenerate(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testName(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0'],
-        ];
     }
 }
