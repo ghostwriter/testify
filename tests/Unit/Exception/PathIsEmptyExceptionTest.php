bee13e9594df23a5a9cd327b62dcf29fa7f5fbab
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:29 -0500

    Update PathIsEmptyExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/PathIsEmptyExceptionTest.php
+++ tests/Unit/Exception/PathIsEmptyExceptionTest.php
@@ -4,75 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Exception;
 
-use Generator;
 use Ghostwriter\Testify\Exception\PathIsEmptyException;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(PathIsEmptyException::class)]
 final class PathIsEmptyExceptionTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(string $message, int $code, Throwable $previous): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    final public function testGetCode(): void
-    {
-        self::assertTrue(true);
-    }
-
-    final public function testGetFile(): void
-    {
-        self::assertTrue(true);
-    }
-
-    final public function testGetLine(): void
-    {
-        self::assertTrue(true);
-    }
-
-    final public function testGetMessage(): void
-    {
-        self::assertTrue(true);
-    }
-
-    final public function testGetPrevious(): void
-    {
-        self::assertTrue(true);
-    }
-
-    final public function testGetTrace(): void
-    {
-        self::assertTrue(true);
-    }
-
-    final public function testGetTraceAsString(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testToString(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testWakeup(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0', 'parameter-1', 'parameter-2'],
-        ];
     }
 }
