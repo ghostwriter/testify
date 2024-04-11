3b85f9247a0ae76d288136a1eda3a3171f44cb0e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:27 -0500

    Update ParameterGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/ParameterGeneratorTest.php
+++ tests/Unit/Generator/ParameterGeneratorTest.php
@@ -4,64 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Generator;
 
-use Generator;
 use Ghostwriter\Testify\Generator\ParameterGenerator;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(ParameterGenerator::class)]
 final class ParameterGeneratorTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(
-        string $name,
-        string $type,
-        bool $isOptional,
-        bool $isVariadic,
-        bool $isPassedByReference,
-        bool $isDefaultValueAvailable,
-        string $defaultValue,
-        array $attributes,
-        array $uses
-    ): void {
-        self::assertTrue(true);
-    }
-
-    public function testGenerate(): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public function testName(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testUses(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => [
-                'parameter-0',
-                'parameter-1',
-                'parameter-2',
-                'parameter-3',
-                'parameter-4',
-                'parameter-5',
-                'parameter-6',
-                'parameter-7',
-                'parameter-8',
-            ],
-        ];
     }
 }
