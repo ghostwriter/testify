16b52e9f337a0d2cf021b2dcf4e95765518f603e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:33 -0500

    Update PathIsNotDirectoryException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/PathIsNotDirectoryException.php
+++ src/Exception/PathIsNotDirectoryException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Testify\Exception;
 
 use RuntimeException;
 
-final class PathIsNotDirectoryException extends RuntimeException
-{
-}
+final class PathIsNotDirectoryException extends RuntimeException {}
