8f258d80b62fe17497337066e78a8aba64000439
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:30 -0500

    Update ClassNameNormalizer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Normalizer/ClassNameNormalizer.php
+++ src/Normalizer/ClassNameNormalizer.php
@@ -7,15 +7,11 @@ namespace Ghostwriter\Testify\Normalizer;
 use Ghostwriter\CaseConverter\CaseConverter;
 use Ghostwriter\Testify\Interface\NormalizerInterface;
 
-use function str_replace;
-use function ucwords;
-
 final readonly class ClassNameNormalizer implements NormalizerInterface
 {
     public function __construct(
         private CaseConverter $caseConverter,
-    ) {
-    }
+    ) {}
 
     public function normalize(string $name): string
     {
