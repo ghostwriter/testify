dd65b19f949e79f1c7d8d1e92e179ccaa8c430b3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:26 -0500

    Update ClassMethodNameNormalizerTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Normalizer/ClassMethodNameNormalizerTest.php
+++ tests/Unit/Normalizer/ClassMethodNameNormalizerTest.php
@@ -4,30 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Normalizer;
 
-use Generator;
 use Ghostwriter\Testify\Normalizer\ClassMethodNameNormalizer;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(ClassMethodNameNormalizer::class)]
 final class ClassMethodNameNormalizerTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestNormalize')]
-    public function testNormalize(string $name): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public static function dataProviderTestNormalize(): Generator
-    {
-        yield from [
-            'testNormalize' => ['parameter-0'],
-        ];
     }
 }
