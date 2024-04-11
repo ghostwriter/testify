96328a5da78b29954f72ac6601a3534e2858176c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:27 -0500

    Update MethodGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/MethodGeneratorTest.php
+++ tests/Unit/Generator/MethodGeneratorTest.php
@@ -4,86 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Generator;
 
-use Generator;
 use Ghostwriter\Testify\Generator\MethodGenerator;
-use Ghostwriter\Testify\Interface\Generator\ClassLikeMemberGeneratorInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(MethodGenerator::class)]
 final class MethodGeneratorTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestCompare')]
-    public function testCompare(ClassLikeMemberGeneratorInterface $right): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(
-        string $name,
-        string $returnType,
-        array $uses,
-        array $parameters,
-        array $body,
-        array $attributes,
-        bool $isStatic,
-        bool $isFinal,
-        bool $isAbstract,
-        bool $isPublic,
-        bool $isProtected,
-        bool $isPrivate,
-        bool $isAnonymous
-    ): void {
-        self::assertTrue(true);
-    }
-
-    public function testGenerate(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testName(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testUses(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestCompare(): Generator
-    {
-        yield from [
-            'testCompare' => ['parameter-0'],
-        ];
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => [
-                'parameter-0',
-                'parameter-1',
-                'parameter-2',
-                'parameter-3',
-                'parameter-4',
-                'parameter-5',
-                'parameter-6',
-                'parameter-7',
-                'parameter-8',
-                'parameter-9',
-                'parameter-10',
-                'parameter-11',
-                'parameter-12',
-            ],
-        ];
     }
 }
