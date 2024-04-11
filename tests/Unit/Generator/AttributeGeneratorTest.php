302721afe8704f74a739ac228b4d8d62b5c38094
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:28 -0500

    Update AttributeGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/AttributeGeneratorTest.php
+++ tests/Unit/Generator/AttributeGeneratorTest.php
@@ -4,35 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Generator;
 
-use Generator;
 use Ghostwriter\Testify\Generator\AttributeGenerator;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(AttributeGenerator::class)]
 final class AttributeGeneratorTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(string $name, array $params): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public function testGenerate(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => ['parameter-0', 'parameter-1'],
-        ];
     }
 }
