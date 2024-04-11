b85bb581909f0e2f3bf5715050286855da460ede
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:27 -0500

    Update ClassGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/ClassGeneratorTest.php
+++ tests/Unit/Generator/ClassGeneratorTest.php
@@ -4,86 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit\Generator;
 
-use Generator;
 use Ghostwriter\Testify\Generator\ClassGenerator;
-use Ghostwriter\Testify\Interface\Generator\ClassLikeGeneratorInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(ClassGenerator::class)]
 final class ClassGeneratorTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestCompare')]
-    public function testCompare(ClassLikeGeneratorInterface $other): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    #[DataProvider('dataProviderTestConstruct')]
-    public function testConstruct(
-        string $name,
-        string $extends,
-        array $attributes,
-        array $uses,
-        array $constants,
-        array $dockBlocks,
-        array $interfaces,
-        array $methods,
-        array $properties,
-        array $traitUses,
-        bool $isAbstract,
-        bool $isFinal,
-        bool $isReadonly
-    ): void {
-        self::assertTrue(true);
-    }
-
-    public function testGenerate(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testName(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public function testUses(): void
-    {
-        self::assertTrue(true);
-    }
-
-    public static function dataProviderTestCompare(): Generator
-    {
-        yield from [
-            'testCompare' => ['parameter-0'],
-        ];
-    }
-
-    public static function dataProviderTestConstruct(): Generator
-    {
-        yield from [
-            'testConstruct' => [
-                'parameter-0',
-                'parameter-1',
-                'parameter-2',
-                'parameter-3',
-                'parameter-4',
-                'parameter-5',
-                'parameter-6',
-                'parameter-7',
-                'parameter-8',
-                'parameter-9',
-                'parameter-10',
-                'parameter-11',
-                'parameter-12',
-            ],
-        ];
     }
 }
