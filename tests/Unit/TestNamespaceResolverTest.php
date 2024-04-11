3f16d019bc04040fcf52bb209573671ad50ace13
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-11 04:43:24 -0500

    Update TestNamespaceResolverTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/TestNamespaceResolverTest.php
+++ tests/Unit/TestNamespaceResolverTest.php
@@ -4,30 +4,15 @@ declare(strict_types=1);
 
 namespace Ghostwriter\TestifyTests\Unit;
 
-use Generator;
 use Ghostwriter\Testify\TestNamespaceResolver;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(TestNamespaceResolver::class)]
 final class TestNamespaceResolverTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        self::markTestSkipped('Not implemented yet.');
-    }
-
-    #[DataProvider('dataProviderTestResolve')]
-    public function testResolve(string $namespace): void
+    public function testExample(): void
     {
         self::assertTrue(true);
-    }
-
-    public static function dataProviderTestResolve(): Generator
-    {
-        yield from [
-            'testResolve' => ['parameter-0'],
-        ];
     }
 }
