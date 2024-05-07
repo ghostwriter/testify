846975ffa9d965fb046b4fe4922ce472431d0fe5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-07 12:16:51 -0500

    Update UseClassGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/UseClassGeneratorTest.php
+++ tests/Unit/Generator/UseClassGeneratorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\TestifyTests\Unit\Generator;
+namespace Tests\Unit\Generator;
 
 use Ghostwriter\Testify\Generator\UseClassGenerator;
 use PHPUnit\Framework\Attributes\CoversClass;
