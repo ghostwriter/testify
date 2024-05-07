11ca86974d50de3fa62a9f043e7651c856bd94fb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-07 12:16:52 -0500

    Update FileGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/FileGeneratorTest.php
+++ tests/Unit/Generator/FileGeneratorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\TestifyTests\Unit\Generator;
+namespace Tests\Unit\Generator;
 
 use Ghostwriter\Testify\Generator\FileGenerator;
 use PHPUnit\Framework\Attributes\CoversClass;
