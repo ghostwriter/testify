5be46c28a58675838ed24a64768f167c4390c62d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-07 12:16:54 -0500

    Update TestDataProviderMethodNameFormatter.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Formatter/TestDataProviderMethodNameFormatter.php
+++ src/Formatter/TestDataProviderMethodNameFormatter.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Ghostwriter\Testify\Formatter;
 
 use Ghostwriter\Testify\Interface\FormatterInterface;
+use Override;
 
 use function sprintf;
 
 final readonly class TestDataProviderMethodNameFormatter implements FormatterInterface
 {
+    #[Override]
     public function format(string $name): string
     {
         return sprintf('dataProvider_%s', $name);
