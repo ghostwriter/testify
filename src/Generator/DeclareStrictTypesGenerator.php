ab75a352c313262ebccd3c0066978b86ef8b9377
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-07 12:16:54 -0500

    Update DeclareStrictTypesGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Generator/DeclareStrictTypesGenerator.php
+++ src/Generator/DeclareStrictTypesGenerator.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Ghostwriter\Testify\Generator;
 
 use Ghostwriter\Testify\Interface\Generator\DeclareStrictTypesGeneratorInterface;
+use Override;
 
 final readonly class DeclareStrictTypesGenerator implements DeclareStrictTypesGeneratorInterface
 {
+    #[Override]
     public function generate(): string
     {
         return 'declare(strict_types=1);';
