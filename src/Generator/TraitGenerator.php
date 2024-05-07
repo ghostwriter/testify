59ffb009321c88d7edd84c88ee102669525416a8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-07 15:23:33 -0500

    Update TraitGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Generator/TraitGenerator.php
+++ src/Generator/TraitGenerator.php
@@ -4,29 +4,34 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Generator;
 
+use Ghostwriter\Testify\Interface\Generator\AttributeGeneratorInterface;
 use Ghostwriter\Testify\Interface\Generator\ClassLike\TraitGeneratorInterface;
 use Ghostwriter\Testify\Interface\Generator\ClassLikeGeneratorInterface;
 use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\ConstantGeneratorInterface;
 use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\MethodGeneratorInterface;
 use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\PropertyGeneratorInterface;
 use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\TraitUseGeneratorInterface;
+use Ghostwriter\Testify\Interface\Generator\UseGeneratorInterface;
 use Override;
 
 final readonly class TraitGenerator implements TraitGeneratorInterface
 {
     /**
-     * @param array<ConstantGeneratorInterface> $constants
-     * @param array<PropertyGeneratorInterface> $properties
-     * @param array<MethodGeneratorInterface>   $methods
-     * @param array<TraitUseGeneratorInterface> $traitUses
+     * @param list<AttributeGeneratorInterface> $attributes
+     * @param list<ConstantGeneratorInterface>  $constants
+     * @param list<MethodGeneratorInterface>    $methods
+     * @param list<PropertyGeneratorInterface>  $properties
+     * @param list<TraitUseGeneratorInterface>  $traitUses
+     * @param list<UseGeneratorInterface>       $uses
      */
     public function __construct(
         private string $name,
-        private array $uses = [],
+        private array $attributes = [],
         private array $constants = [],
-        private array $properties = [],
         private array $methods = [],
+        private array $properties = [],
         private array $traitUses = [],
+        private array $uses = [],
     ) {}
 
     #[Override]
@@ -65,9 +70,17 @@ final readonly class TraitGenerator implements TraitGeneratorInterface
         return $this->name;
     }
 
+    /** @return list<UseGeneratorInterface> */
     #[Override]
     public function uses(): array
     {
         return $this->uses;
+    }
+
+    /** @return list<AttributeGeneratorInterface> */
+    #[Override]
+    public function attributes(): array
+    {
+        return $this->attributes;
     }
 }
