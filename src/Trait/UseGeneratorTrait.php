fe4afe0882b5e6dab305ac7bd824910ecf40a56d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-07 12:16:50 -0500

    Add UseGeneratorTrait.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Trait/UseGeneratorTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Trait;
+
+use Ghostwriter\Testify\Generator\UseClassGenerator;
+use Ghostwriter\Testify\Generator\UseConstantGenerator;
+use Ghostwriter\Testify\Generator\UseFunctionGenerator;
+use Ghostwriter\Testify\Interface\Generator\UseGeneratorInterface;
+
+trait UseGeneratorTrait
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $alias = '',
+    ) {}
+
+    final public function alias(): string
+    {
+        return $this->alias;
+    }
+
+    final public function compare(UseGeneratorInterface $generator): int
+    {
+        return match (true) {
+            $this::class === $generator::class => $this->name <=> $generator->name(),
+            $generator instanceof UseClassGenerator => match (true) {
+                $this instanceof UseConstantGenerator => self::ORDER_AFTER,
+                $this instanceof UseFunctionGenerator => self::ORDER_AFTER,
+                default => self::ORDER_SAME,
+            },
+            $generator instanceof UseFunctionGenerator => match (true) {
+                $this instanceof UseClassGenerator => self::ORDER_BEFORE,
+                $this instanceof UseConstantGenerator => self::ORDER_AFTER,
+                default => self::ORDER_SAME,
+            },
+            $generator instanceof UseConstantGenerator => match (true) {
+                $this instanceof UseClassGenerator => self::ORDER_BEFORE,
+                $this instanceof UseFunctionGenerator => self::ORDER_BEFORE,
+                default => self::ORDER_SAME,
+            },
+            default => self::ORDER_SAME,
+        };
+    }
+
+    final public function name(): string
+    {
+        return $this->name;
+    }
+}
