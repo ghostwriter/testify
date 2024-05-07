4bce8d6cdf2b71abf2d3601525e3a14ef1de658b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-07 12:16:52 -0500

    Update InterfaceGeneratorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Generator/InterfaceGeneratorTest.php
+++ tests/Unit/Generator/InterfaceGeneratorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\TestifyTests\Unit\Generator;
+namespace Tests\Unit\Generator;
 
 use Ghostwriter\Testify\Generator\InterfaceGenerator;
 use PHPUnit\Framework\Attributes\CoversClass;
