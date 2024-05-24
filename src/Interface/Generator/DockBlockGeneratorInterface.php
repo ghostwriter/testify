dcade4cb1558a95052bd70ace25d961659b8bf3e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-24 11:32:44 -0500

    Add DockBlockGeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Generator/DockBlockGeneratorInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Interface\Generator;
+
+use Ghostwriter\Testify\Interface\GeneratorInterface;
+
+interface DockBlockGeneratorInterface extends GeneratorInterface {}
