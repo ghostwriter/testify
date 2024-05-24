9a80322a88c0b4ac7a5ac9247dc7f8b387b1def0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-24 11:32:41 -0500

    Add InterfaceGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/ClassLike/InterfaceGenerator.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator\ClassLike;
+
+use Ghostwriter\Testify\Interface\Generator\ClassLike\InterfaceGeneratorInterface;
+use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\ConstantGeneratorInterface;
+use Ghostwriter\Testify\Interface\Generator\ClassLikeMember\MethodGeneratorInterface;
+use Ghostwriter\Testify\Interface\GeneratorInterface;
+use Ghostwriter\Testify\Trait\ClassLikeGeneratorTrait;
+use Override;
+
+use function implode;
+use function array_reduce;
+
+final class InterfaceGenerator implements InterfaceGeneratorInterface
+{
+    use ClassLikeGeneratorTrait;
+
+    public function addMethod(GeneratorInterface $method): void
+    {
+        $this->methods[] = $method;
+    }
+
+    #[Override]
+    public function generate(): string
+    {
+        $code = 'interface ' . $this->name;
+
+        if ($this->extends !== []) {
+            $code .= ' extends ';
+            //. implode(', ', $this->extends);
+
+            foreach ($this->extends as $extend) {
+                $code .= $extend->generate() . ', ';
+            }
+        }
+
+        $code .= self::NEWLINE . '{' . self::NEWLINES;
+
+        foreach ($this->constants as $constant) {
+            if (! $constant instanceof ConstantGeneratorInterface) {
+                continue;
+            }
+
+            $code .= $constant->generate() . self::NEWLINES;
+        }
+
+        $methods = array_reduce($this->methods, static function (array $methods, GeneratorInterface $method): array {
+            if ($method instanceof MethodGeneratorInterface) {
+                $methods[] = $method;
+            }
+
+            return $methods;
+        }, []);
+
+        foreach ($this->methods as $method) {
+            if (! $method instanceof MethodGeneratorInterface) {
+                continue;
+            }
+
+            $code .= $method->generate() . self::NEWLINES;
+        }
+
+        return $code . '}' . self::NEWLINE;
+    }
+}
