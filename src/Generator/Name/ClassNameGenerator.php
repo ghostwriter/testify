64f81e3cfa9cd2caeed71307089d634e2f44e4f6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-24 11:32:41 -0500

    Add ClassNameGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/Name/ClassNameGenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator\Name;
+
+use Ghostwriter\Testify\Interface\Generator\NameGeneratorInterface;
+use Ghostwriter\Testify\Trait\NameGeneratorTrait;
+
+final readonly class ClassNameGenerator implements NameGeneratorInterface
+{
+    use NameGeneratorTrait;
+}
