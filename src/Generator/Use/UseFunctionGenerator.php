fb59f914cb7b236c8dec6093dd205eb1f23809b0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-24 11:32:42 -0500

    Add UseFunctionGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/Use/UseFunctionGenerator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator\Use;
+
+use Ghostwriter\Testify\Interface\Generator\UseFunctionGeneratorInterface;
+use Ghostwriter\Testify\Trait\UseGeneratorTrait;
+use Override;
+
+final readonly class UseFunctionGenerator implements UseFunctionGeneratorInterface
+{
+    use UseGeneratorTrait;
+
+    #[Override]
+    public function generate(): string
+    {
+        $code = 'use function ' . $this->name;
+
+        if ($this->alias !== '') {
+            $code .= ' as ' . $this->alias;
+        }
+
+        return $code . ';';
+    }
+}
