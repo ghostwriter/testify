dc4568aebfea6aa49494a6ac4e2b9f084fcd529b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 17:41:53 -0500

    Update HandlerInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/HandlerInterface.php
+++ src/Interface/HandlerInterface.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Interface;
 
+use Throwable;
+
 interface HandlerInterface
 {
+    /**
+     * @throws Throwable
+     */
     public function handle(CommandInterface $command): int;
 }
