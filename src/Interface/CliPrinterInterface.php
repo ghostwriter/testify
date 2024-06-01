516da6b20697c7176cf7839e3936d50865577ae9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 17:31:41 -0500

    Add CliPrinterInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/CliPrinterInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Interface;
+
+use Throwable;
+
+interface CliPrinterInterface
+{
+    public function print(CommandInterface $command): string;
+
+    public function printThrowable(Throwable $exception): string;
+}
