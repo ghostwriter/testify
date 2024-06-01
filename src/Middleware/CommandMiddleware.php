e4da70fbff81887ab21f9a432dd377f73a3028fb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 17:41:53 -0500

    Update CommandMiddleware.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Middleware/CommandMiddleware.php
+++ src/Middleware/CommandMiddleware.php
@@ -8,9 +8,16 @@ use Ghostwriter\Testify\Interface\CommandInterface;
 use Ghostwriter\Testify\Interface\HandlerInterface;
 use Ghostwriter\Testify\Interface\MiddlewareInterface;
 use Override;
+use Throwable;
 
 final readonly class CommandMiddleware implements MiddlewareInterface
 {
+    /**
+     * @throws Throwable
+     */
     #[Override]
-    public function process(CommandInterface $command, HandlerInterface $handler): int {}
+    public function process(CommandInterface $command, HandlerInterface $handler): int
+    {
+        return $handler->handle($command);
+    }
 }
