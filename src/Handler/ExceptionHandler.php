66fa7ef18d9af5949c464b5af704ef38931e0e9b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 17:31:41 -0500

    Add ExceptionHandler.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Handler/ExceptionHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Handler;
+
+use Ghostwriter\Testify\Interface\CliPrinterInterface;
+use Ghostwriter\Testify\Interface\CommandInterface;
+use Ghostwriter\Testify\Interface\HandlerInterface;
+use Override;
+
+final readonly class ExceptionHandler implements HandlerInterface
+{
+    public function __construct(
+        private CliPrinterInterface $printer
+    ) {}
+
+    #[Override]
+    public function handle(CommandInterface $command): int
+    {
+        echo $this->printer->print($command);
+
+        return 127;
+    }
+}
