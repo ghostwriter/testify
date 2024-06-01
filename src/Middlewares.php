9989c8d5f06728ee393559c71b136927baf731fb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 17:31:41 -0500

    Add Middlewares.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Middlewares.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify;
+
+use Ghostwriter\Testify\Interface\CommandInterface;
+use Ghostwriter\Testify\Interface\HandlerInterface;
+use Ghostwriter\Testify\Interface\MiddlewareInterface;
+use Override;
+use RuntimeException;
+
+use function array_shift;
+
+final class Middlewares implements MiddlewareInterface
+{
+    /**
+     * @param array<MiddlewareInterface> $middlewares
+     */
+    public function __construct(
+        private array $middlewares = []
+    ) {
+        foreach ($this->middlewares as $middleware) {
+            if (! $middleware instanceof MiddlewareInterface) {
+                throw new RuntimeException('Middleware must implement ' . MiddlewareInterface::class);
+            }
+        }
+    }
+
+    public static function new(MiddlewareInterface ...$middlewares): self
+    {
+        return new self($middlewares);
+    }
+
+    public function add(MiddlewareInterface ...$middlewares): void
+    {
+        $this->middlewares = [...$this->middlewares, ...$middlewares];
+    }
+
+    #[Override]
+    public function process(CommandInterface $command, HandlerInterface $handler): int
+    {
+        if ($this->middlewares === []) {
+            return $handler->handle($command);
+        }
+
+        $middleware = array_shift($this->middlewares);
+
+        if (! $middleware instanceof MiddlewareInterface) {
+            throw new RuntimeException('Middleware must implement Middleware interface');
+        }
+
+        return $middleware->process($command, $handler);
+    }
+}
