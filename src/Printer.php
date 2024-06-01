b6ee6e85bd399bcab825a747382fcfd83ea1db1a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 18:05:41 -0500

    Update Printer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Printer.php
+++ src/Printer.php
@@ -6,9 +6,11 @@ namespace Ghostwriter\Testify;
 
 use Ghostwriter\Testify\Interface\GeneratorInterface;
 use Ghostwriter\Testify\Interface\PrinterInterface;
+use Override;
 
 final readonly class Printer implements PrinterInterface
 {
+    #[Override]
     public function print(GeneratorInterface $generator): string
     {
         return $generator->generate();
