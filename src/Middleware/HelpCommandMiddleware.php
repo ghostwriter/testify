78a9dd1c99ef224a9437dde4c2f00aa12a51df98
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 17:41:53 -0500

    Update HelpCommandMiddleware.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Middleware/HelpCommandMiddleware.php
+++ src/Middleware/HelpCommandMiddleware.php
@@ -9,9 +9,13 @@ use Ghostwriter\Testify\Interface\CommandInterface;
 use Ghostwriter\Testify\Interface\HandlerInterface;
 use Ghostwriter\Testify\Interface\MiddlewareInterface;
 use Override;
+use Throwable;
 
 final class HelpCommandMiddleware implements MiddlewareInterface
 {
+    /**
+     * @throws Throwable
+     */
     #[Override]
     public function process(CommandInterface $command, HandlerInterface $handler): int
     {
