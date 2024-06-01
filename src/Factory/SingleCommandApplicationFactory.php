56213f8a85cf10f25e25bfc5a6d4d454ed0a1cdf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 17:31:40 -0500

    Add SingleCommandApplicationFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Factory/SingleCommandApplicationFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Factory;
+
+use Composer\InstalledVersions;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\FactoryInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\SingleCommandApplication;
+use Throwable;
+use Override;
+
+/**
+ * @implements FactoryInterface<SingleCommandApplication>
+ */
+final readonly class SingleCommandApplicationFactory implements FactoryInterface
+{
+    /**
+     * @throws Throwable
+     */
+    #[Override]
+    public function __invoke(ContainerInterface $container): SingleCommandApplication
+    {
+        $application = $container->build(SingleCommandApplication::class);
+        $application->addArgument('source', InputArgument::OPTIONAL, 'The path to search for missing tests.', 'src');
+        $application->addArgument('tests', InputArgument::OPTIONAL, 'The path used to create tests.', 'tests');
+        $application->addOption('dry-run', 'd', InputOption::VALUE_NONE, 'Do not write any files.');
+        $application->addOption('force', 'f', InputOption::VALUE_NONE, 'Overwrite existing files.');
+        $application->setAutoExit(false);
+        $application->setDescription('Generate missing Tests.');
+        $application->setName('Testify');
+        $application->setVersion(InstalledVersions::getPrettyVersion('ghostwriter/testify') ?? 'UNKNOWN');
+        return $application;
+    }
+}
