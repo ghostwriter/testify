ef039b871affde929aad4c7614a3af61abc0f1cd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-01 18:05:41 -0500

    Update UseGeneratorTrait.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Trait/UseGeneratorTrait.php
+++ src/Trait/UseGeneratorTrait.php
@@ -8,6 +8,7 @@ use Ghostwriter\Testify\Generator\Use\UseClassGenerator;
 use Ghostwriter\Testify\Generator\Use\UseConstantGenerator;
 use Ghostwriter\Testify\Generator\Use\UseFunctionGenerator;
 use Ghostwriter\Testify\Interface\Generator\UseGeneratorInterface;
+use Override;
 
 trait UseGeneratorTrait
 {
@@ -16,18 +17,19 @@ trait UseGeneratorTrait
         public readonly string $alias = '',
     ) {}
 
+    #[Override]
     final public function alias(): string
     {
         return $this->alias;
     }
 
+    #[Override]
     final public function compare(UseGeneratorInterface $generator): int
     {
         return match (true) {
             $this::class === $generator::class => $this->name <=> $generator->name(),
             $generator instanceof UseClassGenerator => match (true) {
-                $this instanceof UseConstantGenerator => self::ORDER_AFTER,
-                $this instanceof UseFunctionGenerator => self::ORDER_AFTER,
+                $this instanceof UseConstantGenerator, $this instanceof UseFunctionGenerator => self::ORDER_AFTER,
                 default => self::ORDER_SAME,
             },
             $generator instanceof UseFunctionGenerator => match (true) {
@@ -36,14 +38,14 @@ trait UseGeneratorTrait
                 default => self::ORDER_SAME,
             },
             $generator instanceof UseConstantGenerator => match (true) {
-                $this instanceof UseClassGenerator => self::ORDER_BEFORE,
-                $this instanceof UseFunctionGenerator => self::ORDER_BEFORE,
+                $this instanceof UseClassGenerator, $this instanceof UseFunctionGenerator => self::ORDER_BEFORE,
                 default => self::ORDER_SAME,
             },
             default => self::ORDER_SAME,
         };
     }
 
+    #[Override]
     final public function name(): string
     {
         return $this->name;
