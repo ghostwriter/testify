5ee3a2bc47adea7d16fbabc9fcf66c9f4544ea61
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 11:02:17 -0500

    Update FailedToDetermineCurrentWorkingDirectoryException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToDetermineCurrentWorkingDirectoryException.php
+++ src/Exception/FailedToDetermineCurrentWorkingDirectoryException.php
@@ -6,4 +6,6 @@ namespace Ghostwriter\Testify\Exception;
 
 use RuntimeException;
 
-final class FailedToDetermineCurrentWorkingDirectoryException extends RuntimeException {}
+final class FailedToDetermineCurrentWorkingDirectoryException extends RuntimeException
+{
+}
