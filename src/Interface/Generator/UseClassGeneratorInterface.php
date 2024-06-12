c21302bc4c3ba3c594009913069b374415a3e40c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 11:02:15 -0500

    Update UseClassGeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/Generator/UseClassGeneratorInterface.php
+++ src/Interface/Generator/UseClassGeneratorInterface.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Interface\Generator;
 
-interface UseClassGeneratorInterface extends UseGeneratorInterface {}
+interface UseClassGeneratorInterface extends UseGeneratorInterface
+{
+}
