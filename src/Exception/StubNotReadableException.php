48616924cc7593f73c130bda352cdd22263363ce
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 11:02:16 -0500

    Update StubNotReadableException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/StubNotReadableException.php
+++ src/Exception/StubNotReadableException.php
@@ -6,4 +6,6 @@ namespace Ghostwriter\Testify\Exception;
 
 use RuntimeException;
 
-final class StubNotReadableException extends RuntimeException {}
+final class StubNotReadableException extends RuntimeException
+{
+}
