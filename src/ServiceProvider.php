fcee9baa5034e31cb1e1f58cbee3e16a45964e68
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-14 21:26:45 -0500

    Update ServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,15 +6,9 @@ namespace Ghostwriter\Testify;
 
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\ServiceProviderInterface;
-use Ghostwriter\Testify\Factory\HandlersFactory;
-use Ghostwriter\Testify\Factory\MiddlewaresFactory;
-use Ghostwriter\Testify\Factory\SingleCommandApplicationFactory;
-use Ghostwriter\Testify\Interface\CliPrinterInterface;
-use Ghostwriter\Testify\Interface\PrinterInterface;
-use Ghostwriter\Testify\Interface\RunnerInterface;
+use Override;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\SingleCommandApplication;
 use Throwable;
 
 final readonly class ServiceProvider implements ServiceProviderInterface
@@ -22,14 +16,9 @@ final readonly class ServiceProvider implements ServiceProviderInterface
     /**
      * @throws Throwable
      */
+    #[Override]
     public function __invoke(ContainerInterface $container): void
     {
-        $container->alias(CliPrinterInterface::class, CliPrinter::class);
-        $container->alias(OutputInterface::class, ConsoleOutput::class);
-        $container->alias(PrinterInterface::class, Printer::class);
-        $container->alias(RunnerInterface::class, Runner::class);
-        $container->factory(Handlers::class, HandlersFactory::class);
-        $container->factory(Middlewares::class, MiddlewaresFactory::class);
-        $container->factory(SingleCommandApplication::class, SingleCommandApplicationFactory::class);
+        $container->alias(ConsoleOutput::class, OutputInterface::class);
     }
 }
