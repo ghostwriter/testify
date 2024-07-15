7008af1b4b03b06eeacfdd52b7cf7b53e80c6d89
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-14 21:26:45 -0500

    Update ExceptionHandler.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Handler/ExceptionHandler.php
+++ src/Handler/ExceptionHandler.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Handler;
 
+use Ghostwriter\Container\Attribute\Inject;
+use Ghostwriter\Testify\CliPrinter;
 use Ghostwriter\Testify\Interface\CliPrinterInterface;
 use Ghostwriter\Testify\Interface\CommandInterface;
 use Ghostwriter\Testify\Interface\HandlerInterface;
@@ -12,6 +14,7 @@ use Override;
 final readonly class ExceptionHandler implements HandlerInterface
 {
     public function __construct(
+        #[Inject(CliPrinter::class)]
         private CliPrinterInterface $printer
     ) {
     }
