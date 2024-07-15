281a183340c299b3f8bbb3a9f115f0a8665f192f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-14 21:26:45 -0500

    Update NotFoundHandler.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Handler/NotFoundHandler.php
+++ src/Handler/NotFoundHandler.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Handler;
 
+use Ghostwriter\Container\Attribute\Inject;
+use Ghostwriter\Testify\CliPrinter;
 use Ghostwriter\Testify\Interface\CliPrinterInterface;
 use Ghostwriter\Testify\Interface\CommandInterface;
 use Ghostwriter\Testify\Interface\HandlerInterface;
+use Override;
 
 use const PHP_EOL;
 
 final readonly class NotFoundHandler implements HandlerInterface
 {
     public function __construct(
+        #[Inject(CliPrinter::class)]
         private CliPrinterInterface $printer
     ) {
     }
