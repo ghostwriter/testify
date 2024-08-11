3ad98d21d774537f2adb76a97d0ea5f9b9d795f4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-11 13:24:35 -0500

    Move TestifyCommandHandlerTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Console/Handler/TestifyCommandHandlerTest.php
+++ tests/Unit/Console/Handler/TestifyCommandHandlerTest.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit\Handler;
+namespace Tests\Unit\Console\Handler;
 
-use Ghostwriter\Testify\Handler\TestifyCommandHandler;
+use Ghostwriter\Testify\Console\Handler\TestifyCommandHandler;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
