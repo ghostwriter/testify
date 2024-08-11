ea4448781232f464b7a71e5446190097e2dec029
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-11 13:24:34 -0500

    Move ExceptionHandlerTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Console/Handler/ExceptionHandlerTest.php
+++ tests/Unit/Console/Handler/ExceptionHandlerTest.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit\Handler;
+namespace Tests\Unit\Console\Handler;
 
-use Ghostwriter\Testify\Handler\ExceptionHandler;
+use Ghostwriter\Testify\Console\Handler\ExceptionHandler;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
