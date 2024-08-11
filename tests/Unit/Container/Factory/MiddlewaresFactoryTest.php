c422f4a9741e0f708351dc43ffce8313193d3552
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-11 13:24:37 -0500

    Move MiddlewaresFactoryTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Container/Factory/MiddlewaresFactoryTest.php
+++ tests/Unit/Container/Factory/MiddlewaresFactoryTest.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit\Factory;
+namespace Tests\Unit\Container\Factory;
 
-use Ghostwriter\Testify\Factory\MiddlewaresFactory;
+use Ghostwriter\Testify\Container\Factory\MiddlewaresFactory;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
