dde0cc8a0991ab3ecee4229bd5f955e54370ad12
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-11 13:24:32 -0500

    Move HelpCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Console/Command/HelpCommand.php
+++ src/Console/Command/HelpCommand.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Testify\Command;
+namespace Ghostwriter\Testify\Console\Command;
 
 use Ghostwriter\Testify\Interface\CommandInterface;
 use Override;
