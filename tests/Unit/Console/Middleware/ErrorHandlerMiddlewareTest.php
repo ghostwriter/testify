13b4bfa0717df1d97d709d3438c1d8cc5e05e2c7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-11 13:24:36 -0500

    Move ErrorHandlerMiddlewareTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Console/Middleware/ErrorHandlerMiddlewareTest.php
+++ tests/Unit/Console/Middleware/ErrorHandlerMiddlewareTest.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit\Middleware;
+namespace Tests\Unit\Console\Middleware;
 
-use Ghostwriter\Testify\Middleware\ErrorHandlerMiddleware;
+use Ghostwriter\Testify\Console\Middleware\ErrorHandlerMiddleware;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
