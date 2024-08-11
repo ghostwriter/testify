36f9b46ad976f3a14a0dea7d4cda6325c9d7c6fb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-11 02:53:55 -0500

    Update ServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,8 +7,6 @@ namespace Ghostwriter\Testify;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\ServiceProviderInterface;
 use Override;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
 
 final readonly class ServiceProvider implements ServiceProviderInterface
@@ -19,6 +17,5 @@ final readonly class ServiceProvider implements ServiceProviderInterface
     #[Override]
     public function __invoke(ContainerInterface $container): void
     {
-        $container->alias(ConsoleOutput::class, OutputInterface::class);
     }
 }
