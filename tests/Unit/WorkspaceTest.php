ed54a5ab8a05cdea25aa7f54bfab7a244249a5d5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-11 13:24:39 -0500

    Move WorkspaceTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/WorkspaceTest.php
+++ tests/Unit/WorkspaceTest.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-use Ghostwriter\Testify\Project;
+use Ghostwriter\Testify\Workspace;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
-#[CoversClass(Project::class)]
-final class ProjectTest extends TestCase
+#[CoversClass(Workspace::class)]
+final class WorkspaceTest extends TestCase
 {
     public function testExample(): void
     {
