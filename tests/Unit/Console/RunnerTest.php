065b40656a00868590484edfff361b180a97d3e2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-11 13:24:36 -0500

    Move RunnerTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Console/RunnerTest.php
+++ tests/Unit/Console/RunnerTest.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit;
+namespace Tests\Unit\Console;
 
-use Ghostwriter\Testify\Runner;
+use Ghostwriter\Testify\Console\Runner;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
