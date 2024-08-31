49a322d3141bf2b03295e232d017ef61a779beb6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-31 11:00:50 -0500

    Move AttributeGeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Generator/AttributeGeneratorInterface.php
+++ src/Generator/AttributeGeneratorInterface.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Testify\Interface\Generator;
-
-use Ghostwriter\Testify\Interface\GeneratorInterface;
+namespace Ghostwriter\Testify\Generator;
 
 interface AttributeGeneratorInterface extends GeneratorInterface
 {
