5df1d35e431021a7bd3c36f60a23f6d792a884ce
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-31 11:00:51 -0500

    Add InterfaceGeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Generator/ClassLike/InterfaceGeneratorInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Generator\ClassLike;
+
+use Ghostwriter\Testify\Generator\ClassLikeGeneratorInterface;
+
+interface InterfaceGeneratorInterface extends ClassLikeGeneratorInterface
+{
+}
