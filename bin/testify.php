4222c2238232a9c20bf86d1216a2399dbf14faa0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-10 11:46:50 -0500

    Update testify.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- bin/testify.php
+++ bin/testify.php
@@ -9,18 +9,12 @@ use Ghostwriter\Testify\Testify;
 use const DIRECTORY_SEPARATOR;
 use const STDERR;
 
-use function dirname;
-use function file_exists;
-use function fwrite;
-use function implode;
-use function sprintf;
-
 /** @var ?string $_composer_autoload_path */
 (static function (string $composerAutoloadPath): never {
-    if (! file_exists($composerAutoloadPath)) {
-        fwrite(
+    if (! \file_exists($composerAutoloadPath)) {
+        \fwrite(
             STDERR,
-            sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath)
+            \sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath)
         );
 
         exit(1);
@@ -32,4 +26,4 @@ use function sprintf;
      * #BlackLivesMatter.
      */
     exit(Testify::new()->run());
-})($_composer_autoload_path ?? implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), 'vendor', 'autoload.php']));
+})($_composer_autoload_path ?? \implode(DIRECTORY_SEPARATOR, [\dirname(__DIR__), 'vendor', 'autoload.php']));
