09086ff5638203f72de6e7b884aa560a52bf0c7c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-15 15:37:52 -0600

    Move SingleCommandApplicationFactoryTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Container/Factory/SingleCommandApplicationFactoryTest.php
+++ tests/Unit/Container/Factory/SingleCommandApplicationFactoryTest.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Container\Factory;
 
-use Ghostwriter\Testify\Container\Factory\MiddlewaresFactory;
+use Ghostwriter\Testify\Container\Factory\SingleCommandApplicationFactory;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
-#[CoversClass(MiddlewaresFactory::class)]
-final class MiddlewaresFactoryTest extends TestCase
+#[CoversClass(SingleCommandApplicationFactory::class)]
+final class SingleCommandApplicationFactoryTest extends TestCase
 {
     public function testExample(): void
     {
