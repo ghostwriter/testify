9283ae718aa3bacaa4b24e830142e6ed6adf33a2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-15 15:37:49 -0600

    Add StringTypeInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Type/Atomic/Scalar/StringTypeInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Type\Atomic\Scalar;
+
+use Ghostwriter\Testify\Type\Atomic\ScalarTypeInterface;
+
+interface StringTypeInterface extends ScalarTypeInterface
+{
+}
