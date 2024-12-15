f2e64b9d9a3e83df030ad75378248ddcfb14a6ed
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-15 15:37:31 -0600

    Move ExceptionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Exception;
 
-use RuntimeException;
+use Throwable;
 
-final class PathIsEmptyException extends RuntimeException
+interface ExceptionInterface extends Throwable
 {
 }
