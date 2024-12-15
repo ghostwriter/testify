64df7bd4c289fe997aebad93d7660e94d7302b79
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-15 15:37:52 -0600

    Move ArgvFactoryTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Container/Factory/ArgvFactoryTest.php
+++ tests/Unit/Container/Factory/ArgvFactoryTest.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Container\Factory;
 
-use Ghostwriter\Testify\Container\Factory\HandlersFactory;
+use Ghostwriter\Testify\Container\Factory\ArgvFactory;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
-#[CoversClass(HandlersFactory::class)]
-final class HandlersFactoryTest extends TestCase
+#[CoversClass(ArgvFactory::class)]
+final class ArgvFactoryTest extends TestCase
 {
     public function testExample(): void
     {
