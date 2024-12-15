7e5038a08a2526ac85d0d59e6132c29fb5872ee4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-15 15:37:31 -0600

    Add SingleCommandApplicationFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Container/Factory/SingleCommandApplicationFactory.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Testify\Container\Factory;
+
+use Composer\InstalledVersions;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\FactoryInterface;
+use Override;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\SingleCommandApplication;
+use Throwable;
+
+/**
+ * @implements FactoryInterface<SingleCommandApplication>
+ */
+final readonly class SingleCommandApplicationFactory implements FactoryInterface
+{
+    /**
+     * @throws Throwable
+     */
+    #[Override]
+    public function __invoke(ContainerInterface $container): SingleCommandApplication
+    {
+        $singleCommandApplication = $container->build(SingleCommandApplication::class);
+
+        $singleCommandApplication->addArgument(
+            'source',
+            InputArgument::OPTIONAL,
+            'The path to search for missing tests.',
+            'src'
+        );
+
+        $singleCommandApplication->addArgument(
+            'tests',
+            InputArgument::OPTIONAL,
+            'The path used to create tests.',
+            'tests'
+        );
+
+        $singleCommandApplication->addOption('dry-run', 'd', InputOption::VALUE_NONE, 'Do not write any files.');
+
+        $singleCommandApplication->addOption('force', 'f', InputOption::VALUE_NONE, 'Overwrite existing files.');
+
+        $singleCommandApplication->setAutoExit(false);
+
+        $singleCommandApplication->setDescription('Generate missing Tests.');
+
+        $singleCommandApplication->setName('Testify');
+
+        $singleCommandApplication->setVersion(InstalledVersions::getPrettyVersion('ghostwriter/testify'));
+
+        return $singleCommandApplication;
+    }
+}
