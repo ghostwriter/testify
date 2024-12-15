25940eb468c5101aabed0731a5294860c769d257
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-15 15:37:30 -0600

    Update ConfigExtension.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Container/Extension/ConfigExtension.php
+++ src/Container/Extension/ConfigExtension.php
@@ -7,7 +7,9 @@ namespace Ghostwriter\Testify\Container\Extension;
 use Ghostwriter\Config\Contract\ConfigInterface;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\ExtensionInterface;
+use Ghostwriter\Testify\Exception\ShouldNotHappenException;
 use Override;
+use RuntimeException;
 
 /**
  * @implements ExtensionInterface<ConfigInterface>
@@ -18,43 +20,64 @@ final readonly class ConfigExtension implements ExtensionInterface
      * Extend a service on the given container.
      *
      * @param ConfigInterface $service
-     *
      */
     #[Override]
     public function __invoke(ContainerInterface $container, object $service): ConfigInterface
     {
-        global $argv;
-        //        $short_opts = "df";
-        //        $long_opts = [
-        //            'source' => 'The path to search for missing tests.',
-        //            'tests' => 'The path used to create tests.',
-        //            'dry-run' => 'Whether to write the files or not',
-        //            'force' => 'Whether to overwrite existing files',
-        //        ];
+        //    0 - No colon - no argument ( boolean flag )
+        //    1 - One colon - argument required ( string )
+        //    2 - Two colons - argument optional ( string|null )
 
-        //        $app->setDescription('Generate missing Tests.');
-        //        $app->setName('Testify');
-        //        $app->setVersion(InstalledVersions::getPrettyVersion('ghostwriter/testify') ?? 'UNKNOWN');
+        $short_options = '';
+        $long_options = [];
+        $defaults = [
+            'h|help' => 0,
+            'V|version' => 0,
+            'd|dry-run' => 0,
+            'f|force' => 0,
+            'r|required' => 1,
+            'o|optional' => 2,
+        ];
 
-        $rest_index = -1;
-        $opts = \getopt('df', ['dry-run', 'force'], $rest_index);
-        $options = \array_slice($argv, $rest_index);
+        foreach ($defaults as $option => $argument) {
+            [$short, $long] = \explode('|', $option);
 
-        $service->set('source', $options[0] ?? 'src');
-        $service->set('tests', $options[1] ?? 'tests');
-        $service->set('dryRun', (
-            \array_key_exists('d', $opts)
-            || \array_key_exists('dry-run', $opts)
-            || \in_array('-d', $options, true)
-            || \in_array('--dry-run', $options, true)
-        ));
-        $service->set('force', (
-            \array_key_exists('f', $opts)
-            || \array_key_exists('force', $opts)
-            || \in_array('-f', $options, true)
-            || \in_array('--force', $options, true)
-        ));
+            $type = match ($argument) {
+                0 => '',
+                1 => ':',
+                2 => '::',
+                default => throw new ShouldNotHappenException(\sprintf(
+                    'Invalid argument type: %s, expected 0, 1 or 2',
+                    $argument,
+                )),
+            };
+
+            $short_options .= $short . $type;
+
+            $long_options[] = $long . $type;
+        }
+
+        $options = \getopt($short_options, $long_options, $rest_index);
+
+        if ($options === false) {
+            throw new RuntimeException('Failed to parse options');
+        }
+
+        $service->set('dryRun', \array_key_exists('d', $options) || \array_key_exists('dry-run', $options));
+
+        $service->set('force', \array_key_exists('f', $options) || \array_key_exists('force', $options));
+
+        $argv = \array_slice($_SERVER['argv'] ?? [], $rest_index);
+
+        $service->set('source', $argv[0] ?? 'src');
+
+        $service->set('tests', $argv[1] ?? 'tests');
 
         return $service;
+    }
+
+    private function highlight(string $thing): string
+    {
+        return \sprintf("\x1B[1;32m%s\x1B[0m", $thing);
     }
 }
