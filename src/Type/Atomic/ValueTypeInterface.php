528f59526b24c19d51613808b43fad36ded8712c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-07 20:32:33 -0600

    Update ValueTypeInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Type/Atomic/ValueTypeInterface.php
+++ src/Type/Atomic/ValueTypeInterface.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Testify\Type\Atomic;
 
 use Ghostwriter\Testify\Type\AtomicTypeInterface;
 
-interface ValueTypeInterface extends AtomicTypeInterface
-{
-}
+interface ValueTypeInterface extends AtomicTypeInterface {}
