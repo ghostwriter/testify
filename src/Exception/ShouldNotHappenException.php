64117acbda60f145b75ea8d3fb6ddc06c5c54dbc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-07 20:32:26 -0600

    Update ShouldNotHappenException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/ShouldNotHappenException.php
+++ src/Exception/ShouldNotHappenException.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Testify\Exception;
 
 use LogicException;
 
-final class ShouldNotHappenException extends LogicException implements ExceptionInterface
-{
-}
+final class ShouldNotHappenException extends LogicException implements ExceptionInterface {}
