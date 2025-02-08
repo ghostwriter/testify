296d00805565093ede2dc8bca6baaf5b83f848fe
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-07 20:32:29 -0600

    Update UseFunctionGeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Generator/Use/UseFunctionGeneratorInterface.php
+++ src/Generator/Use/UseFunctionGeneratorInterface.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Testify\Generator\Use;
 
-interface UseFunctionGeneratorInterface extends UseGeneratorInterface
-{
-}
+interface UseFunctionGeneratorInterface extends UseGeneratorInterface {}
