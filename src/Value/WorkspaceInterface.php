9cc14dd8ff47f1dcc590a22d189af87666b508d8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-07 20:32:34 -0600

    Update WorkspaceInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/WorkspaceInterface.php
+++ src/Value/WorkspaceInterface.php
@@ -6,4 +6,15 @@ namespace Ghostwriter\Testify\Value;
 
 interface WorkspaceInterface
 {
+    public function dryRun(): bool;
+
+    public function fixture(): string;
+
+    public function force(): bool;
+
+    public function source(): string;
+
+    public function tests(): string;
+
+    public function vendor(): string;
 }
