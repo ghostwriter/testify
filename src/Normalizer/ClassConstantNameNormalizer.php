406dd71bfdccc34a26bf6e3979965ba800290cdf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-07 20:32:30 -0600

    Update ClassConstantNameNormalizer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Normalizer/ClassConstantNameNormalizer.php
+++ src/Normalizer/ClassConstantNameNormalizer.php
@@ -11,8 +11,7 @@ final readonly class ClassConstantNameNormalizer implements NormalizerInterface
 {
     public function __construct(
         private CaseConverter $caseConverter,
-    ) {
-    }
+    ) {}
 
     #[Override]
     public function normalize(string $name): string
