4ea9c9f4ee875188557575a2cf5eaf9b8eac9bf8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-07 20:32:29 -0600

    Update StaticCallGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Generator/StaticCallGenerator.php
+++ src/Generator/StaticCallGenerator.php
@@ -6,28 +6,31 @@ namespace Ghostwriter\Testify\Generator;
 
 use Override;
 
+use function mb_rtrim;
+use function sprintf;
+use function str_contains;
+
 final readonly class StaticCallGenerator implements GeneratorInterface
 {
     public function __construct(
         private string $class,
         private string $method,
         private array $args = [],
-    ) {
-    }
+    ) {}
 
     #[Override]
     public function generate(): string
     {
-        $code = \sprintf('%s::%s(', $this->class, $this->method);
+        $code = sprintf('%s::%s(', $this->class, $this->method);
 
         foreach ($this->args as $arg) {
-            if (\str_contains((string) $arg, ' ')) {
-                $arg = \sprintf("'%s'", $arg);
+            if (str_contains((string) $arg, ' ')) {
+                $arg = sprintf("'%s'", $arg);
             }
 
             $code .= $arg . ', ';
         }
 
-        return \sprintf('%s);%s', \rtrim($code, ', '), self::NEWLINE);
+        return sprintf('%s);%s', mb_rtrim($code, ', '), self::NEWLINE);
     }
 }
