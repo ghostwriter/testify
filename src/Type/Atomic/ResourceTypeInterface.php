d1dd9948fbf888eef4564dae41c97a1f6ec254c2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-07 20:32:32 -0600

    Update ResourceTypeInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Type/Atomic/ResourceTypeInterface.php
+++ src/Type/Atomic/ResourceTypeInterface.php
@@ -6,6 +6,4 @@ namespace Ghostwriter\Testify\Type\Atomic;
 
 use Ghostwriter\Testify\Type\AtomicTypeInterface;
 
-interface ResourceTypeInterface extends AtomicTypeInterface
-{
-}
+interface ResourceTypeInterface extends AtomicTypeInterface {}
