e2165c2b21dfdf668efad5fea160da3d00cdf5e5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-07 20:32:31 -0600

    Update Runner.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Runner/Runner.php
+++ src/Runner/Runner.php
@@ -5,18 +5,22 @@ declare(strict_types=1);
 namespace Ghostwriter\Testify\Runner;
 
 use Generator;
+use Ghostwriter\Filesystem\Interface\FilesystemInterface;
 use Ghostwriter\Testify\Application\PhpFileFinder;
 use Ghostwriter\Testify\Value\WorkspaceInterface;
 use Override;
 
 use const DIRECTORY_SEPARATOR;
 
+use function dd;
+use function str_replace;
+
 final readonly class Runner implements RunnerInterface
 {
     public function __construct(
+        private FilesystemInterface $filesystem,
         private PhpFileFinder $phpFileFinder,
-    ) {
-    }
+    ) {}
 
     #[Override]
     public function run(WorkspaceInterface $workspace): Generator
@@ -25,8 +29,14 @@ final readonly class Runner implements RunnerInterface
 
         $unitTestsDirectory = $workspace->tests() . DIRECTORY_SEPARATOR . 'unit';
 
+        if (! $this->filesystem->isDirectory($unitTestsDirectory)) {
+
+            dd($unitTestsDirectory);
+            //            $this->filesystem->cleanDirectory($unitTestsDirectory);
+        }
+
         foreach ($this->phpFileFinder->find($sourceDirectory) as $file) {
-            yield $file => \str_replace([$sourceDirectory, '.php'], [$unitTestsDirectory, 'Test.php'], $file);
+            yield $file => str_replace([$sourceDirectory, '.php'], [$unitTestsDirectory, 'Test.php'], $file);
         }
     }
 }
