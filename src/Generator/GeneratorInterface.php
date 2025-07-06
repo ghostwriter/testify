340ae836dcc2b377fee6a27061d57c4e13c74fe2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-07-06 12:25:09 -0500

    Update GeneratorInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Generator/GeneratorInterface.php
+++ src/Generator/GeneratorInterface.php
@@ -6,19 +6,19 @@ namespace Ghostwriter\Testify\Generator;
 
 interface GeneratorInterface
 {
-    public const int ORDER_BEFORE = -1;
-
-    public const int ORDER_AFTER = 1;
-
-    public const int ORDER_SAME = 0;
-
     public const string INDENT = '    ';
-
-    public const string SPACE = ' ';
 
     public const string NEWLINE = "\n";
 
     public const string NEWLINES = "\n\n";
+
+    public const int ORDER_AFTER = 1;
+
+    public const int ORDER_BEFORE = -1;
+
+    public const int ORDER_SAME = 0;
+
+    public const string SPACE = ' ';
 
     public function generate(): string;
 }
